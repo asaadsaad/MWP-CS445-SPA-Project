5a53f8bf10ef54d5a6b2367bb36dea95cf023862
Moe-Hassan123 <mohamedalfidil123@gmail.com>
2019-11-25 15:59:46 -0800

    loging logout function code

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-// your code here
+window.onload = function () {
+
+    //  variable area 
+
+    let loged = false;
+
+    let loginglook = ` <h1> Please login </h1>
+    Username: <input id="UserName" type="text" value= "mwp"></br>
+    Password: <input id="password" type="text"value= "123"></br>
+    <button type="button" id="loging">Login</button>`;
+
+    let animationLook = `<h1 id="status">"Start" </h1>
+    <button type="button" id="refresh">Refresh Animayion</button>
+    <button type="button" id="logout">logout</button>`;
+
+    let divLook = document.querySelector("#outlet");
+    divLook.innerHTML = loginglook;
+
+    //  for the login page 
+    let login = document.querySelector("#loging")
+    let UserName = document.querySelector("#UserName")
+    let password = document.querySelector("#password")
+
+    //  for the animation page 
+    // let RefreshAnim = document.querySelector("#refresh")
+    // let logout = document.querySelector("#logout")
+
+    // addEventListener login page 
+    login.addEventListener("click", logingFunc);
+
+    // addEventListener login page 
+    function addEvent() {
+        if (loged) {
+            let RefreshAnim = document.querySelector("#refresh");
+            let logout = document.querySelector("#logout");
+
+            RefreshAnim.addEventListener("click", refreshFunc);
+            logout.addEventListener("click", logoutFunc);
+        }
+    }
+
+
+    // creating function
+    function logingFunc() {
+        alert("logggggggg")
+        divLook.innerHTML = animationLook;
+        loged = true;
+        addEvent()
+    }
+
+    function refreshFunc() {
+        alert("reeeee")
+    }
+
+    function logoutFunc() {
+        alert("outttttt")
+        divLook.innerHTML = loginglook;
+    }
+
+
+}
