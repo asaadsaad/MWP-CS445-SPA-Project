a8fc4a28f0d71ef107993fc884d641d89778adf0
Bereket Teklu <bteklu@mum.edu>
2019-11-25 22:56:10 -0600

    script.js updated

--- script.js
+++ script.js
@@ -2,5 +2,17 @@
 
 window.onload = app;
 function app () {
+    <div class="page">
+        <div class="form">
+            <form class="main-form">
+                <h2>LogIn</h2>
+                <input type="text" id="uname" placeholder="user name" value="mwp"/>
+                <input type="text" id="pass" placeholder="password" value="123"/>
+
+                <input type="button" onclick="logins()" value="Login" />
+                <div id="outp"></div>
+            </form>
+        </div>
+    </div>
 
 }
