35b1b20f1b73e3c72aa782f989030b5c578e93e5
Seble Aygoda <saygoda@mum.edu>
2019-11-26 15:54:18 -0600

    login

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// your code here
+window.onload=function(){
+
+    const login=
+    `<h1>Please Login</h1>
+    User name:<input></input><br><br>
+    Password:  <input></input><br><br>
+    <button>Login</button>`
+    document.getElementById("outlet").innerHTML=login
+
+
+
+}
+
