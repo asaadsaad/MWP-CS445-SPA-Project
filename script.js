b5b853c390ebcab1ee2c33a882a7201efeecef7c
Seble Aygoda <saygoda@mum.edu>
2019-11-26 20:26:41 -0600

    cc

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ window.onload=function(){
     `<h1>Please Login</h1>
     User name:<input id="default1"></input><br><br>
     Password:  <input id="default2"></input><br><br>
-    <button id="buttton">Login</button>`
+    <button id="button">Login</button>`
     document.getElementById("outlet").innerHTML=login
     document.getElementById("default1").value="mwp";
     document.getElementById("default2").value="123";
     let button=document.getElementById("button");
     //when our login button clicked
     function loginClicked(){
-       location.replace("http://127.0.0.1:5500/firstproject/MWP-CS452-2019-SPA-Project/index.html")
+       window.location.assign("http://127.0.0.1:5500/firstproject/MWP-CS452-2019-SPA-Project/index.html");
     }
     button.addEventListener("click",loginClicked);
 
