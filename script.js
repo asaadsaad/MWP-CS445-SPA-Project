04cfd4f187fc3ca5bff1527e7a8c65aecdf641c7
Tariku Gelan <tgelan@mum.edu>
2019-11-26 23:27:58 -0600

    my first commit, please refer to my read me file which i am going to update everyday

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-// your code here
+/* eslint-disable require-jsdoc */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable strict */
+window.onload = function () {
+    const loginPage = `<h1> Please login</h1>
+Username <input type="text" placeholder="mwp" value = "mwp"/> <br>
+Password <input type="text" placeholder="123" value = "123"/> <br>
+<button id ="logInButton">Login</button> <br>`;
+    const animationPage = `
+        <textarea name=" Welcome all from " id="" cols="60" rows="20"></textarea><br>
+        <button id ="Refanimation">Refresh Animation</button> 
+        <button id ="logoutid">Logout</button> <br>`;
+
+    let outlet = document.getElementById("outlet");
+    outlet.innerHTML = loginPage; //autoloading log in page
+
+    let loginBtn = document.getElementById("logInButton");
+    loginBtn.addEventListener("click", nextPage);  //to go to animation page
+    
+    function nextPage() {
+        outlet.innerHTML = animationPage;
+        redirectingPage();                //to load the second page
+    }
+    function redirectingPage() {
+        let logOutBTN = document.getElementById("logoutid");
+        //  let tex
+        logOutBTN.addEventListener("click", function () {
+            outlet.innerHTML = loginPage;
+        })
+
+    }
+}
