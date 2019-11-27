2bdeb13e4c15dce169899a01ae6b44b6e68ef0bb
nLeul <leulnecha@gmail.com>
2019-11-26 20:16:36 -0600

    login page formed

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // your code here
+window.onload = page;
+function page() {
+    const loginPage =
+        `<h3>Please Login</h3>Username <input id="username" type="text"><br>
+Password <input id="password" type="text"><br>
+<button id="login">Login</button>`
+
+    const animationPage = `    <h3>welcome All from<h3 id="location"></h3>
+                </h3>
+                <textarea id="textarea" cols="50" rows="15"></textarea><br>
+                <button id="refAnimBtn">Refresh Animation</button>
+                <button id=" logoutBtn">Logout</button>`;
+
+
+    const outlet = document.getElementById("outlet");
+    outlet.innerHTML = loginPage;
+    const loginBtn = document.getElementById("login");
+    loginBtn.addEventListener("click", loadAnimationPage);
+    function loadAnimationPage() {
+        outlet.innerHTML = animationPage;
+
+    }
+
+
+}
+
+
