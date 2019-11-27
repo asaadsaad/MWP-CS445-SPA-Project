6548d91d4a3e91a3351c3df2d6193a6ae8637040
Daniel <demamu@outlook.com>
2019-11-27 17:56:41 -0600

    Login page

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// your code here
+
+
+
+
+    
+
+  window.onload = function() {
+    loginPage = `<h1>Please Login</h1>
+    <br />
+    <form id="myLoginForm">
+    <label>
+    <span><b>Username</b></span>
+    <input type='text' id='userName' value='mwp' required/> 
+    </label>
+    <br />   
+    <label>
+    <span><b>Password</b></span>     
+    <input type='text' id='userPassword' value='123' required/> 
+    </label>
+    <br />  
+    <button type="submit">login</button>
+     </form>`;
+    document.getElementById("outlet").innerHTML = loginPage;
+    
+    
+} 
