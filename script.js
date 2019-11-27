093296a373d84d999edd08d26536b4d253b16838
Ashim Ghimire <aghimire85@gmail.com>
2019-11-27 17:30:03 -0600

    got the fetch functionality and got the animation to move

--- script.js
+++ script.js
@@ -7,27 +7,34 @@ window.onload = function () {
     let longitude;
     let latitude;
     let token;
+    let allAnimation;
+    let moveAnimation;
 
 
-    //Create a HTML Page
+    //Push the HTML code to the HTML Page
     let loginPage = `<h1>Please Login</h1><br>
     Username: <input type="text" id="username" value="mwp"><br>
     Password: <input type="text" id="password" value="123"><br>
     <input type="button" id="login" value="Login">`
 
-    let animationPage = `<h1>Welcome all from</h1>
+    let animationPage = `<h1>Welcome All</h1>
     <h2 id="geolocation"></h2>
     <textarea rows="40" cols="100" id="animation"></textarea><br>
     <button type="button" id="refresh">Refresh Animation</button>
     <button type="button" id="logout">Logout</button>`
 
+
+    //Call the login function
     login();
+
+
+
     //function to login to the animation
     function login() {
         //Get the DOM Element to create Login Page
         let outlet = document.querySelector("#outlet");
         outlet.innerHTML = loginPage;
-        history.pushState("login", "login", "?login")
+        history.pushState("myLogin", "login", "?login")
 
         //Get the DOM Element for Login Page
         let loginButton = document.querySelector("#login");
@@ -39,7 +46,7 @@ window.onload = function () {
 
     }
 
-
+    //Function to get the animation Page and the geolocation
     function animEventList() {
         //Get the geolocation
         const location = navigator.geolocation.getCurrentPosition(success, fail);
@@ -48,9 +55,16 @@ window.onload = function () {
         function success(position) {
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
-            console.log(longitude);
-            console.log(latitude);
             currentPosition();
+            let geolocation = document.querySelector("#geolocation");
+            let animation = document.querySelector("#animation");
+            let refresh = document.querySelector("#refresh");
+            let logout = document.querySelector("#logout");
+
+            //Create an EvenListener once inside the animation page
+            //refresh.addEventListener("click", getNewAnimation);
+            logout.addEventListener("click", logoutOfAnimation);
+            refresh.addEventListener("click", clearLastAnimation);
         }
 
         function fail(msg) {
@@ -61,14 +75,7 @@ window.onload = function () {
 
 
         //Get the DOM Elements once inside the animation page
-        let geolocation = document.querySelector("#geolocation");
-        let animation = document.querySelector("#animation");
-        let refresh = document.querySelector("#refresh");
-        let logout = document.querySelector("#logout");
 
-        //Create an EvenListener once inside the animation page
-        //refresh.addEventListener("click", getNewAnimation);
-        logout.addEventListener("click", logoutOfAnimation);
 
     }
 
@@ -77,12 +84,36 @@ window.onload = function () {
 
     //To login
     function insideLogin() {
-        history.pushState("animation", "animation", "?animation")
+        history.pushState("myAnimation", "animation", "?animation")
         outlet.innerHTML = animationPage;
         isLogin = true;
 
         animEventList()
+        getAnimation();
     }
+    //Function for making the string animate
+    function movingAnimation() {
+        const animArray = allAnimation.split("=====\n");
+        animation.innerHTML = animArray[0];
+        let currentAnim = 0;
+        let maxlength = animArray.length;
+        moveAnimation = setInterval(() => {
+            animation.innerHTML = animArray[currentAnim];
+            currentAnim++;
+            if (currentAnim === maxlength) {
+                currentAnim = 0;
+            }
+        }, 200);
+    }
+
+    //Clear the last animation
+    function clearLastAnimation() {
+        clearInterval(moveAnimation)
+        getAnimation();
+
+    }
+
+
 
 
 
@@ -118,15 +149,34 @@ window.onload = function () {
     }
 
     async function currentPosition() {
-        const result = await fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${navigatorKey}&location=${latitude},${longitude}&includeRoadMetadata=true&includeNearestIntersection=true`, {
+        const result = await fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${navigatorKey}&location=${latitude},${longitude}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
             }
         })
         const myPosition = await result.json();
-        console.log(myPosition);
-        geolocation.innerHTML = `${myPosition.results[0].locations[0].adminArea5}, ${myPosition.results[0].locations[0].adminArea3} ${myPosition.results[0].locations[0].adminArea1}`;
+        const city = myPosition.results[0].locations[0].adminArea5;
+        const state = myPosition.results[0].locations[0].adminArea3;
+        const country = myPosition.results[0].locations[0].adminArea1;
+        geolocation.innerHTML = `Thanks for accessing from ${city}, ${state} ${country}`;
+
+    }
+
+    async function getAnimation() {
+        const result = await fetch(`http://mumstudents.org/api/animation`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/text",
+                Authorization: `Bearer ${token}`
+
+            }
+
+
+        })
+        allAnimation = await result.text();
+        movingAnimation();
+
 
     }
 
