5bc382c907092e12211125699f16652a28d2b363
Seble Aygoda <saygoda@mum.edu>
2019-11-26 19:50:52 -0600

    change

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ window.onload=function(){
 
     const login=
     `<h1>Please Login</h1>
-    User name:<input></input><br><br>
-    Password:  <input></input><br><br>
-    <button>Login</button>`
+    User name:<input id="default1"></input><br><br>
+    Password:  <input id="default2"></input><br><br>
+    <button id="buttton">Login</button>`
     document.getElementById("outlet").innerHTML=login
-
-
+    document.getElementById("default1").value="mwp";
+    document.getElementById("default2").value="123";
+    let button=document.getElementById("button");
+    
+    // function buttonClick(){
+    //    location.replace()
+    // }
 
 }
 
