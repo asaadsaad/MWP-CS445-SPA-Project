2476553973dde086762f2242c5ac74347da61072
Bereket Teklu <bteklu@mum.edu>
2019-11-26 22:53:21 -0600

    script.js updated

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 window.onload = main;
-function main () {
-    <div class="page">
-        <div class="form">
-            <form class="main-form">
-                <h2>LogIn</h2>
-                <input type="text" id="uname" placeholder="user name" value="mwp"/>
-                <input type="text" id="pass" placeholder="password" value="123"/>
+function main() {
 
-                <input type="button" onclick="logins()" value="Login" />
-                <div id="outp"></div>
-            </form>
-        </div>
-    </div>
+    // outlet declaration
+    const out = document.querySelector('#outlet');
+
+    const logins = `
+        <h2>Please LogIn</h2>
+        User name : <input type="text" id="uname" placeholder="username" value="mwp" />
+        Password : <input type="text" id="pass" placeholder="password" value="123" />
+        <input type="button" id="lgin" value="Login"/>`;
+
+    const animations = `
+    <div id="location">WELCOME to ASCII Animation SPA</div> 
+    <textarea id="animation" style="font-size: 15px;" rows="55" cols="35" ></textarea><br>
+    <input type="button" id="refr" value="Refresh Animation" />
+    <input type="button" id="lout" value="Log Out" />`;
+    // create two routes
+    let MyRouters = function (name, routers) {
+        return
+        ({
+            name: name,
+            routers: routers
+        });
+    };
+
+    let myRouts = new MyRouters("myRouts",
+        [
+            {
+                path: "/",
+                name: "Login - Page"
+            },
+            {
+                path: "/animation",
+                name: "Animation - Page"
+            }
+        ]);
+
+    let currPath = window.location.pathname;
+    if (currPath === "/") {
+        out.innerHTML = logins;
+    } if (currPath === "/animation") {
+        out.innerHTML = animations;
+    }
 
 }
