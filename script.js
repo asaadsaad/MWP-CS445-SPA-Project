fdc963ee01f8d339aa114f4b18a5d2772404e034
Derese92 <54817093+Derese92@users.noreply.github.com>
2019-11-27 14:50:14 -0600

    Day One

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// your code here
+
+
+const login = `
+<h1>Please Login</h1><br>
+Username:<input placeholder="mwp" value="mwp"/><br/>
+Password:<input placeholder="123" value="123"/><br/>
+<button id="loginbtn">Login</button>
+`
+let loginTemplate = document.querySelector("#outlet");
+loginTemplate.innerHTML = login;
+document.getElementById("loginbtn").addEventListener("click", logToAnimationpage)
+
+const animation = `
+<div id="address"> Welcome to SPA Animation</div>
+<textarea id="animation" rows="25" cols="25" style="font-size: 20px"></textarea><br><br>
+<button id="refresh">Refresh Animation</button>
+<button id="logout">Logout</button>        
+`
+let animationTemplate = document.querySelector("#outlet");
+loginTemplate.innerHTML = animation;
+
+// function logToAnimationpage(){
+//     loginTemplate.innerHTML = animation;
+
+// }
+
+
+fetch("http://mumstudents.org/api/login", {
+method: "POST",
+headers: {
+    "content-type": "application/json"
+},
+body: JSON.stringify(
+    {
+    username: "mwp",
+    password: "123"
+            }
+) 
+})
+.then(resp =>resp.json())
+.then(console.log)
