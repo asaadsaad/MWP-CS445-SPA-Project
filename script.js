72c06e01a80a0cb0bd11a0bd362f312d6786d4d9
Weldensie Embaye <wembaye@mum.edu>
2019-11-26 20:01:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-// your code here
+
+window.onload = function animationProject() {
+
+    let myLogin = `username: <input type="text" id="input1" /> <br/>
+             password: <input type="text" id="input2" /> <br/>
+             <button type="button" id="login">login</button>`
+
+
+    document.getElementById("outlet").innerHTML = myLogin;
+
+    let tokenId, strId;
+
+    document.getElementById("login").addEventListener('click', async function () {
+        response = await fetch('http://mumstudents.org/api/login',
+            {
+                method: 'POST',
+                headers: { 'content-Type': 'application/json' },
+                body: JSON.stringify({ username: "mwp", password: "123" })
+            })
+        data = await response.json();
+        tokenId = data.token;
+
+
+        response2 = await fetch('http://mumstudents.org/api/animation',
+            {
+                method: 'GET',
+                headers: { "Authorization": `Bearer ${tokenId}` },
+            })
+
+        resStr = await response2.text();
+        strId = resStr;
+
+        document.getElementById("outlet").innerHTML = strId;
+
+
+
+    //     response3 = await fetch('http://open.mapquestapi.com/geocoding/v1/reverse',
+    //     {
+            
+    //     })
+
+    // resStr = await response3.text();
+    // strId = resStr;
+
+    // document.getElementById("outlet").innerHTML = strId;
+
+    }
+
+    )
+
+
+
+
+}
+
+
+
