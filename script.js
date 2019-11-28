319a134bd37195384fba7c186d88a0f4818aa0fc
Temesgen <temehayet124@gmail.com>
2019-11-27 18:54:58 -0600

    first part completed:

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+window.onload = function(){
+  let loginpage = `<h1> Please Login </h1><br>
+      Username: <input type="text" value="mwp"></input> <br> 
+      Password: <input type="text" value="123"></input> <br>
+      <button type="button" id="loginBtn"> login </button>`
+  document.querySelector("#outlet").innerHTML = loginpage;
+}
