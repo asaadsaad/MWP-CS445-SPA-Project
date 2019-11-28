98a4fdd0e687fd2bfde0de51333659e5f866f2c9
Daniel <demamu@outlook.com>
2019-11-28 14:37:22 -0600

    token secssesfully pulled

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
-
-
-
-    
-
   window.onload = function() {
+
+    let token = null,
+    animation = null,
+    userLatitude = 0,
+    userLongitude = 0,
+
     loginPage = `<h1>Please Login</h1>
     <br />
     <form id="myLoginForm">
@@ -20,7 +20,43 @@
     <br />  
     <button type="submit">login</button>
      </form>`;
+
     document.getElementById("outlet").innerHTML = loginPage;
-    
-    
-} 
+
+      document.getElementById("myLoginForm").onsubmit = async function(event) {
+      event.preventDefault();
+
+      let userName = document.querySelector("#userName").value;
+      let userPassword = document.querySelector("#userPassword").value;
+      let restoken = await getToken("http://mumstudents.org/api/login", {
+
+        username: userName,
+        password: userPassword
+
+      });
+
+      token = restoken.token;
+      console.log(token);
+      
+    };
+
+    async function getToken(url = "", data = {}) {
+      
+      const response = await fetch(url, {
+        method: "POST", 
+        mode: "cors", 
+        cache: "no-cache", 
+        credentials: "same-origin", 
+        headers: {
+          "Content-Type": "application/json"
+          
+        },
+        redirect: "follow", 
+        referrer: "no-referrer", 
+        body: JSON.stringify(data) 
+      });
+      return await response.json(); 
+    }
+  
+
+  }
