41067fdb5ed86e5cc41b95408f3a08ca0e7afe5d
Derese92 <54817093+Derese92@users.noreply.github.com>
2019-11-28 00:16:49 -0600

    Second Day Progress

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+window.onload = function(){
+//let token;
 
 const login = `
 <h1>Please Login</h1><br>
@@ -8,34 +9,69 @@ Password:<input placeholder="123" value="123"/><br/>
 `
 let loginTemplate = document.querySelector("#outlet");
 loginTemplate.innerHTML = login;
-document.getElementById("loginbtn").addEventListener("click", logToAnimationpage)
+let loginId = document.getElementById("loginbtn");
+loginId.addEventListener("click", logToAnimationpage);
 
 const animation = `
-<div id="address"> Welcome to SPA Animation</div>
-<textarea id="animation" rows="25" cols="25" style="font-size: 20px"></textarea><br><br>
-<button id="refresh">Refresh Animation</button>
-<button id="logout">Logout</button>        
-`
-let animationTemplate = document.querySelector("#outlet");
-loginTemplate.innerHTML = animation;
+        <div id="address"> Welcome to SPA Animation</div>
+        <textarea id="animation" rows="22" cols="22" style="font-size: 20px"></textarea><br><br>
+        <button id="refresh">Refresh Animation</button>
+        <button id="logout">Logout</button>        
+        `
+// let animationTemplate = document.querySelector("#outlet");
+// loginTemplate.innerHTML = animation;
 
-// function logToAnimationpage(){
-//     loginTemplate.innerHTML = animation;
+function logToAnimationpage() {
+    loginTemplate.innerHTML = animation;
+    logout();
+    refresh();
+}
+// let logoutId = document.getElementById("logout");
+// logoutId.addEventListener("click",logToLoginpage);
+function logout() {
+    document.getElementById("logout").addEventListener("click", logoutto)
+    function logoutto() {
+        loginTemplate.innerHTML = login;
+    }
+}
+function refresh() {
+    document.getElementById("refresh").addEventListener("click", refreshfunc)
+    let textarea = document.getElementById("animation");
+    function refreshfunc() {
+        textarea.innerHTML = animFrames;
 
-// }
+    }
+}
+
+function fetchToken() {
+    fetch("http://mumstudents.org/api/login", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            username: "mwp",
+            password: "123"
+        })
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            fetch("http://mumstudents.org/api/animation", {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Bearer ${data.token}`
+                }
+            })
+            .then(reps => reps.text())
+            .then(data => {
+                console.log(data);
+                animFrames = data.split("=====");
+                //console.log(animFrames);
+            });
+        });
+}
+
+fetchToken();
 
 
-fetch("http://mumstudents.org/api/login", {
-method: "POST",
-headers: {
-    "content-type": "application/json"
-},
-body: JSON.stringify(
-    {
-    username: "mwp",
-    password: "123"
-            }
-) 
-})
-.then(resp =>resp.json())
-.then(console.log)
+}
