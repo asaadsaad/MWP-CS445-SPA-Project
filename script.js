ff98c337cabfe95a0c8ad5841d23ef90ed567442
Weldensie Embaye <wembaye@mum.edu>
2019-11-28 12:33:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,19 @@
 window.onload = function animationProject() {
 
     let myLogin = `<h1>Please refresh and login </h1><br/>
-     username: <input type="text" value="mwp" id="input1" /> <br/>
+             username: <input type="text" value="mwp" id="input1" /> <br/>
              password: <input type="text" value="123" id="input2" /> <br/>
              <button type="button" id="login">login</button>`
 
 
     let myAnimation = `<h1 id="locationTitle"> </h1>
-    <textarea type="text" id="outputDiv" cols="60" rows="15" style="font-size: 18px;"></textarea></br>
+             <textarea type="text" id="outputDiv" cols="60" rows="15" style="font-size: 18px;"></textarea></br>
              <button type="button" id="animation">Refresh Animation</button>
              <button type="button" id="logout">Logout</button>`
 
 
     document.getElementById("outlet").innerHTML = myLogin;
-    let tokenId, animationID, longitude, latitude, keyId = "8SdUOIMUJzlXswkGbSVInlBgxi17ejOI";
+    let tokenId, animationID, textId, longitude, latitude, keyId = "8SdUOIMUJzlXswkGbSVInlBgxi17ejOI";
 
 
     /**
@@ -37,13 +37,13 @@ window.onload = function animationProject() {
 
 
         /**
-         *This function request the user to access his/her location
+        *This function request the user to display his/her geolocation
         * It takes two parameters
-             * @param  {function} success is the first function parameter
-             * @param  {function} fail is the second function paramter
-             * @returns {object} geolocation information if successfully accessed the user location
-             * @returns {error} message if fails to locate the user
-             */
+        * @param  {function} success is the first function parameter
+        * @param  {function} fail is the second function paramter
+        * @returns {object} geolocation information if successfully accessed the user location
+        * @returns {error} message if fails to locate the user
+        */
         navigator.geolocation.getCurrentPosition(success, fail)
 
         function fail() {
@@ -53,18 +53,17 @@ window.onload = function animationProject() {
         function success(position) {
             longitude = (position.coords.longitude).toFixed(6);
             latitude = (position.coords.latitude).toFixed(6);
-            findLocation();
+
 
             async function findLocation() {
                 try {
                     /**
-                     * This function fetch (get) allow to get the geolocation address of the user
-                     *  based on access id, the latitude and longitude information
-                     * @param  {string} is an id to allow access to the server
-                     * @param  {number} is latitude information of the user
-                     * @param  {number} is longitude information of the user
-                     * @returns (JSON) is an JSON geolocation information
-                     */
+                    * This function fetch (get) allow to get the geolocation address of the user ased on access id, the latitude and longitude information
+                   * @param  {string} is an id to allow access to the server
+                   * @param  {number} is latitude information of the user
+                   * @param  {number} is longitude information of the user
+                   * @returns (JSON) is an JSON geolocation information
+                    */
                     let response1 = await fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${keyId}&location=${latitude},${longitude}`,
                         {
                             method: 'GET',
@@ -78,22 +77,24 @@ window.onload = function animationProject() {
                     document.getElementById('locationTitle').innerHTML = `Welcome all from ${city}, ${state},${country}`;
                 }
                 /**
-            * This function handle error in case geolocation information request go wrong.
-            * @param  {error} error message
-            * @returns (error) return error message .
-            */
+               * This function handle error in case geolocation information request go wrong.
+                * @param  {error} error message
+                * @returns (error) return error message .
+               */
                 catch (err) {
                     console.log(err);
                 }
             }
+            findLocation();
         }
 
         try {
             /**
-            * This function fetch (post) the user information to the server
-            * @param  {object} includes method and headers about the user
-            * @returns (JSON) token id once the server request is accepted.
-            */
+        * This function fetch (post) the user information to the server
+        * @param  {object} includes method and headers about the user
+        * @returns (JSON) token id once the server request is accepted.
+         */
+
             response2 = await fetch(`http://mumstudents.org/api/login`,
                 {
                     method: `POST`,
@@ -126,21 +127,21 @@ window.onload = function animationProject() {
                 * @param  {object} includes method and headers about the user
                 * @returns (text) is an animation once the server request is accepted.
                 */
-                response2 = await fetch('http://mumstudents.org/api/animation',
+                response3 = await fetch('http://mumstudents.org/api/animation',
                     {
                         method: 'GET',
                         headers: { 'Content-Type': 'application/text', Authorization: `Bearer ${tokenId}` },
                     })
 
-                strId = await response2.text();
+                textId = await response3.text();
                 playAnimation();
                 async function playAnimation() {
-                    strId = await strId.split("=====\n");
+                    textId = await textId.split("=====\n");
                     let count = 0;
                     animationID = await setInterval(() => {
-                        document.getElementById("outputDiv").innerHTML = strId[count];
+                        document.getElementById("outputDiv").innerHTML = textId[count];
                         count++
-                        if (count == strId.length) {
+                        if (count == textId.length) {
                             count = 0;
                         }
                     }, 200)
@@ -156,13 +157,13 @@ window.onload = function animationProject() {
             }
         }
         /**
-            * I add refresh animation listener that accepts two parameters.
-            * @param  {"click"} is the first parameter that allow the user to clear the refresh button.
-            * @param  {function } is the second parameter that does not take any parameter.
-            * @returns {function} this first return function clear the existing animation.
-            * @returns {function} this second return function call the get animation function and play new animation.
+         * I add refresh animation listener that accepts two parameters.
+         * @param  {"click"} is the first parameter that allow the user to clear the refresh button.
+         * @param  {function } is the second parameter that does not take any parameter.
+         * @returns {function} this first return function clear the existing animation.
+         * @returns {function} this second return function call the get animation function and play new animation.
 
-            */
+         */
         animation.addEventListener("click", async function () {
             await clearInterval(animationID);
             getAnimation();
