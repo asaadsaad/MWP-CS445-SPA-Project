401ea2d2f1755c7a439dc15c2221591e2ff498c1
Temesgen <temehayet124@gmail.com>
2019-11-28 14:56:06 -0600

    update for today

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 window.onload = function(){
   let loginpage = `<h1> Please Login </h1><br>
-      Username: <input type="text" value="mwp"></input> <br> 
-      Password: <input type="text" value="123"></input> <br>
+      Username: <input type="text" id = "userName"  value="mwp"></input> <br> 
+      Password: <input type="text" id = "userpass" value="123"></input> <br>
       <button type="button" id="loginBtn"> login </button>`
   document.querySelector("#outlet").innerHTML = loginpage;
-}
+  
+  document.getElementById("loginBtn").addEventListener("click", login)
+
+    function login() {
+
+      let username = document.querySelector("#userName").value;
+      let password = document.querySelector("#userpass").value;
+      let login = document.querySelector("#loginBtn").value;
+    
+
+       let result= fetch('http://mumstudents.org/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "username":value, "password":value})
+        })
+            .then(response(response.json())
+            .then(data =>({
+                myObj = data
+
+              })
+          }
+        }
