c4a6b91714264f4b44c9bd6ed988875a52d62b8b
Tariku Gelan <tgelan@mum.edu>
2019-11-27 21:52:50 -0600

    second commit on fetch api's

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ i was able to go to next page on click of the first page
 i was also be able to log out and go back to the previus out let page(log in page)
 i will work on the animation page, and fetching stuff tommorrow
 i will email all the problems i have faced tommorow
+
+today 11/26/19
+today i have tried to work on fetching the token and fetching the 
+animation and i was able to display the animation on the text area
+i am planing to do the geolcation functionality and the rest for thursday, on thanks giving day....
+i will email all the problems i have faced during fetching the animation
--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 /* eslint-disable quotes */
 /* eslint-disable strict */
 window.onload = function () {
+    //let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBd…w8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA"
     const loginPage = `<h1> Please login</h1>
 Username <input type="text" placeholder="mwp" value = "mwp"/> <br>
 Password <input type="text" placeholder="123" value = "123"/> <br>
 <button id ="logInButton">Login</button> <br>`;
     const animationPage = `
-        <textarea name=" Welcome all from " id="" cols="60" rows="20"></textarea><br>
+        <textarea name=" Welcome all from " id= "animTextArea" cols="60" rows="20"></textarea><br>
         <button id ="Refanimation">Refresh Animation</button> 
         <button id ="logoutid">Logout</button> <br>`;
 
@@ -17,17 +18,51 @@ Password <input type="text" placeholder="123" value = "123"/> <br>
 
     let loginBtn = document.getElementById("logInButton");
     loginBtn.addEventListener("click", nextPage);  //to go to animation page
-    
+
     function nextPage() {
         outlet.innerHTML = animationPage;
-        redirectingPage();                //to load the second page
+        redirectingPage();
+        refFunction()
+        //fetchForAnim()    //to load the second page
     }
     function redirectingPage() {
         let myLogOut = document.getElementById("logoutid");
         //  let tex
         myLogOut.addEventListener("click", function () {
-            outlet.innerHTML = loginPage;
+            outlet.innerHTML = loginPage
+        })
+    }
+
+    function refFunction() {
+        let txtarAnimPage = document.getElementById("animTextArea");
+        let refButn = document.getElementById("Refanimation");
+
+        refButn.addEventListener("click", function () {
+            txtarAnimPage.innerHTML = displayAnim;
         })
 
     }
+    function fetcForToken() {
+        fetch("http://mumstudents.org/api/login",
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: "mwp", password: "123" })
+            })
+            .then(res => res.json())
+            .then(elem => {
+                fetch("http://mumstudents.org/api/animation", {
+                    method: 'GET',
+                    headers: { "Authorization": `Bearer ${elem.token}` },
+
+                })
+                    .then(res => res.text())
+                    .then(elem => {
+                        displayAnim = elem.split("=====")
+                        console.log(elem)
+                    })
+            })
+    }
+    fetcForToken();
+
 }
