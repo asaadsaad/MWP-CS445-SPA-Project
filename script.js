e5b90c704606cc3bd020810078f491950dd71f0e
semateame <54817239+semateame@users.noreply.github.com>
2019-11-27 21:09:15 -0800

    day2

--- script.js
+++ script.js
@@ -1,57 +1,94 @@
+let token;
+let timeid;
+let i = 0;
 
+function SPA() {
+    let animation = `<div id="adress">Welcome to SPA Animation</div>
+<textarea name="" id="animation" cols="50" rows="20" style="font-size: 18px;"></textarea> <br>
+<button id="refresh">Refresh Animation</button>
+<button id="logout">Logout</button>`
 
-
-
-function application() {
-    let login = `<h2> Please log in </h2>
+    function getlogin() {
+        
+        let login = `<h2> Please log in </h2>
         user name: <input type="text" id="username" value="mwp"><br>
         Passwword: <input type="text" id="password" value="123"><br>
         <button id="button"> login </button>`
-    document.getElementById("outlet").innerHTML = login;
+        document.getElementById("outlet").innerHTML = login;
+        
+    }
+    getlogin();
+
+    function getanimationpage() {
+
+        document.getElementById("outlet").innerHTML = animation
+        document.getElementById("logout").addEventListener("click", getlogin)
+        document.getElementById("refresh").addEventListener("click", gettoken);
+    }
+
+    document.getElementById("button").addEventListener("click", getanimationpage)
+
+
+    let gettoken = async function () {
+
+        let result = await fetch("http://mumstudents.org/api/login",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(sema)
+            })
+        let resp = await result.json()
+        token = resp
+        
+        
+        final();
+        
+
+    }
+
+
+
+
+    let final = async function () {
+
+        let anime = await fetch("http://mumstudents.org/api/animation",
+            {
+                headers: {
+                    "Authorization": ` Bearer ${token.token}`
+                }
+            })
+        let response = await anime.text()
+        let y = response.split("=====\n")
+        clearInterval(timeid);
+        i = 0
+       timeid= setInterval(function () {
+            document.getElementById("animation").innerHTML = y[i]
+            i++
+            if (i === y.length) {
+                i = 0
+            }
+
+        }, 200)
+
+
+
+    }
+
+
+
+
+
+
 
 }
+SPA()
 
-application();
+
+
+
 
 let sema = {
     username: document.getElementById("username").value,
     password: document.getElementById("password").value
 }
-//console.log(sema)
-
-function gettoken() {
-    let token;
-    return fetch("http://mumstudents.org/api/login ",
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(sema)
-        })
-}
-
-
-gettoken().then((response) => { return response.json() })
-    .then((obj) => {
-        token = obj;
-        console.log(token)
-    });
-
-
-
-function getanimationpage() {
-    let animation = `<div id="adress">Welcome to SPA Animation</div>
-<textarea name="" id="animation" cols="50" rows="20" style="font-size: 18px;"></textarea> <br>
-<button id="refresh">Refresh Animation</button>
-<button id="logout">Logout</button>`
-    document.getElementById("outlet").innerHTML = animation
-
-}
-
-
-document.getElementById("button").addEventListener("click", getanimationpage)
-document.getElementById("button").addEventListener("click",)
-
-
-
-
 
