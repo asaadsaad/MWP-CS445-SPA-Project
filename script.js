f3d392ca3a40bc887bdf0a530da91f7767af6f94
yonas emha <yyemha1@gmail.com>
2019-11-28 17:53:47 -0600

    second geolocation

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function SPA() {
     // select
     let outlet = document.querySelector("#outlet");
     let token;
-    let animation, animationId;
-    
+    let timerId;
+
 
 
     // login template
@@ -18,7 +18,7 @@ function SPA() {
     <button id="login">Login</button>
     `
     // animation template
-    let animationTemplate = `<div id="location" style="font-size :20px">Welcome message from</div>
+    let animationTemplate = `<div id="location" style="font-size :20px">welcome to my animation spa</div>
     <textarea id="display" row="40" cols="40" style="font-size:20px"></textarea><br>
     <button id="refresh" >Refresh Animation</button>
     <button id="logout">Logout</button>
@@ -43,35 +43,79 @@ function SPA() {
         })
         let response = await res.json();
         token = response.token;
-        animationpage();
+        animationPage();
         getPost();
+
     }
 
     let getPost = async function () {
-        let res1 = await fetch("http://www.mumstudents.org/api/animation", {
+        if (timerId) {
+            clearInterval(timerId)
+        }
+        let resolve = await fetch("http://www.mumstudents.org/api/animation", {
             headers: {
                 "Authorization": `Bearer ${token}`,
             }
         })
 
-        let data = await res1.text();
+        let data = await resolve.text();
         let array = data.split("=====\n")
         console.log(array)
-        document.querySelector('#display').innerHTML= array;
+        let index = 0;
+        timerId = setInterval(() => {
+            document.querySelector("#display").value = array[index];
+            index++;
+            if (index == array.length) {
+                index = 0;
+            }
+        }, 200)
+
+        //document.querySelector('#display').innerHTML = array;
 
     }
 
-    function login() {
+    // Find location of the user using API key & location(longitude & latitude) - reverse geocode
+
+    function location() {
+
+        navigator.geolocation.getCurrentPosition(success, fail);
+
+        async function success(position) {
+            let key = "Q6NBLIK5NWGiVhHZwd4vZyiw4A1IgD9Z";
+            let lng = position.coords.longitude;
+            let lat = position.coords.latitude;
+            console.log(position)
+            let address = await fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${lat},${lng}&includeRoadMetadata=true&includeNearestIntersection=true`)
+
+            address = await address.json();
+            let city = address.results[0].locations[0].adminArea5;
+            let state = address.results[0].locations[0].adminArea3;
+            let country = address.results[0].locations[0].adminArea1;
+            document.querySelector("#location").innerHTML=`welcome all from ${city},${state},${country}`;
+
+        }
+
+
+        function fail(msg) {
+            console.log(msg.code + msg.message);
+        }
+
+    }
+
+    function loginPage() {
         outlet.innerHTML = loginTemplate;
+        
     }
-    login()
+    loginPage()
     document.querySelector("#login").addEventListener("click", addPost);
 
-    function animationpage() {
+    function animationPage() {
         outlet.innerHTML = animationTemplate;
-        document.querySelector("#logout").addEventListener("click", login);
-        document.querySelector("#refresh").addEventListener("click", getPost)
+        document.querySelector("#logout").addEventListener("click", loginPage);
+        document.querySelector("#refresh").addEventListener("click", getPost);
+        location()
+        
+    
     }
-
 
 }
