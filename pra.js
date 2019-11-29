f13e837acc0f29b50f985cfdd808e68656ae8618
frehiwot <frehiwottm@gmail.com>
2019-11-29 00:33:54 -0600

    fetching geolocation api and finsihing the overall html structure

---
+++ pra.js
@@ -0,0 +1,66 @@
+
+let count = 0;
+window.onload = function () {
+    let login_template = `<h1>Pleas Login</h1>
+<label >User Name </label> <input type =text id = text2>
+<br>
+<br>
+<label >Password </label> <input type =text id="text2">
+<br>
+ <br>
+<button id="btn">Login</button>`
+
+
+    let animation_template =`<textarea id="ta" cols="100%" rows="35%" ></textarea>
+    <br>
+    </br>
+   <button id="but2"> Animation</button> </br></br> <button id="but3">Logout</button>`
+
+    
+    document.getElementById("outlet").innerHTML = login_template;
+    document.getElementById("btn").addEventListener("click", loginbutton);
+
+    function loginbutton() {
+
+        document.getElementById("outlet").innerHTML = animation_template
+    document.getElementById("but2").addEventListener("click", loginbutton)
+        let mytext;
+        async function myfunc() {
+            response = await fetch("http://mumstudents.org/api/login",
+                {
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        "username": "mwp",
+                        "password": "123"
+                    })
+
+                })
+            data = await response.json();
+
+            res = await fetch("http://mumstudents.org/api/animation",
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${data.token}` }
+
+                })
+            result = await res.text();
+            mytext = result;
+
+            let y = mytext.split("=====")
+
+
+            setInterval(function () {
+
+                document.getElementById("ta").innerHTML = y[count]
+                count++;
+                if (count === y.length) {
+                    count = 0;
+                }
+            }, 500)
+
+        }
+
+        myfunc()
+    }
+}
--- script.js
+++ script.js
@@ -1,66 +1,72 @@
+window.onload = function (){
+let animationData;
+let data;
+let animationId
+  // over all html structure
+  let animationTemplate= `<textarea  id="textAreaId" cols="90%" rows="45%"></textarea>
+            <br>
+            <br>
+           <button id="animation_id"> Refresh Animation </button>
+           
+           <button id="logout_id"> LogOut </button> `
+           
+  let loginTemplate = `<h1>Pleas Login</h1>
+                    <label>User Name</label>
+                    <input id="input_id" placeholder="wmp"> </input><br> <br>
+                    <label>Password </label>
+                    <input id="input_id" placeholder="123"> </input><br><br><button id="login_id">login</button> `
 
-let count = 0;
-window.onload = function () {
-    let login_template = `<h1>Pleas Login</h1>
-<label >User Name </label> <input type =text id = text2>
-<br>
-<br>
-<label >Password </label> <input type =text id="text2">
-<br>
- <br>
-<button id="btn">Login</button>`
+document.getElementById("outlet").innerHTML=loginTemplate
 
 
-    let animation_template =`<textarea id="ta" cols="100%" rows="35%" ></textarea>
-    <br>
-    </br>
-   <button id="but2"> Animation</button> </br></br> <button id="but3">Logout</button>`
-
+// feching from the server
+async function login() {
+    //autorization 
+    let response1 = await fetch("http://mumstudents.org/api/login",
+        {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "username": "mwp", "password": "123" })
+        })
+    data = await response1.json();
+    document.getElementById("outlet").innerHTML=animationTemplate
     
-    document.getElementById("outlet").innerHTML = login_template;
-    document.getElementById("btn").addEventListener("click", loginbutton);
+   
+    getAnimation()
+//accesed token
+    }   
 
-    function loginbutton() {
-
-        document.getElementById("outlet").innerHTML = animation_template
-    document.getElementById("but2").addEventListener("click", loginbutton)
-        let mytext;
-        async function myfunc() {
-            response = await fetch("http://mumstudents.org/api/login",
-                {
-                    method: "POST",
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        "username": "mwp",
-                        "password": "123"
-                    })
-
-                })
-            data = await response.json();
-
-            res = await fetch("http://mumstudents.org/api/animation",
-                {
-                    method: "GET",
-                    headers: { "Authorization": `Bearer ${data.token}` }
-
-                })
-            result = await res.text();
-            mytext = result;
-
-            let y = mytext.split("=====")
+     async function getAnimation(){
+    let response2 = await fetch("http://mumstudents.org/api/animation",
+        {
+            method: "Get",
+            headers: { "Authorization": `Bearer ${data.token}` }
 
 
-            setInterval(function () {
+        })
+    animationData = await response2.text() 
+    clearInterval(animationId);
+   
+    document.getElementById("animation_id").addEventListener("click",getAnimation)
+    let animationsplit = animationData.split("=====") 
+    let count=0;
+    animationId = setInterval(function(){
+        document.getElementById("textAreaId").innerHTML=animationsplit[count] 
+        ++count;
+         if(count===animationsplit.length){
+            count=0;   
+         }
+    },200)
+    
 
-                document.getElementById("ta").innerHTML = y[count]
-                count++;
-                if (count === y.length) {
-                    count = 0;
-                }
-            }, 500)
-
-        }
-
-        myfunc()
     }
-}
+   
+ document.getElementById("login_id").addEventListener("click",login)
+                   
+   
+   
+   
+    }//onload finish here
+
+
+
