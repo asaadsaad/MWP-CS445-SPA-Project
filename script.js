5effaba629bbdcbc036789238da4598494125b86
Bereket Teklu <bteklu@mum.edu>
2019-11-28 23:52:35 -0600

    script.js updated

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
 window.onload = main;
 
 function main() {
+    const myGeoLocKey = "bQDwCHvfs2jJ3B1Fpc8QGyMAFGU4AxrW";
+    let status = document.querySelector("#geoloc");
+    let myLat, myLon;
+    let questMapU = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${myGeoLocKey}&location=${myLat},${myLon}`;
+    let animationFrame, tokenAnim, timers;
+    let isRefre = false;
     // outlet declaration
     const out = document.querySelector('#outlet');
     // store token url address
@@ -13,8 +19,9 @@ function main() {
     Password: <input type="text" id="password" value="123"><br><br>
     <button type="button" id="lgin" >Login</button>`
 
-    const animations = `<h1>Welcome all from</h1>
-    <h2 id="geoloc"></h2>
+    const animations = `
+    <h2>welcome</h2>
+    <h3 id="geoloc"></h3>
     <textarea rows="30" cols="80" id="animationDisp"></textarea><br>
     <button type="button" id="refresh">Refresh Animation</button>
     <button type="button" id="logout">Logout</button>`
@@ -28,18 +35,75 @@ function main() {
         out.innerHTML = logins;
         // get element from the DOM for login 
         let loggingBtn = document.querySelector("#lgin");
+        // access location
+        navigator.geolocation.getCurrentPosition(function (currPos) {
+            myLon = currPos.coords.longitude;
+            myLat = currPos.coords.latitude;
+            locFetching();
+
+        }, function (message) {
+            alert("CAN'T access your location, It is Blocked!!!")
+        });
+        // 
+        tokenFunction();
         //creates EventListener for the login button directs to the animation UI page
         loggingBtn.addEventListener("click", animationUI);
+        animFetching();
+        isRefre = true;
+
         // pushes a new state to history
         history.pushState("loggingUI", "Login History", "/index.html");
     } // end of loggingUI function
 
+    // fetches current location
+    async function locFetching() {
+        let fetchLoc, getLoc, city, state, country;
+        
+        fetchLoc = await fetch(questMapU,
+            {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+        getLoc = await fetchLoc.json()
+
+        city = getLoc.outcomes[0].fetchLoc[0].adminArea5;
+        state = getLoc.outcomes[0].fetchLoc[0].adminArea3;
+        country = getLoc.outcomes[0].fetchLoc[0].adminArea1;
+
+        status.innerHTML = `all from, ${city}, ${state}, ${country}`
+    }
+    // fetches animation 
+    async function animFetching() {
+        let jsonRes;
+        const response = await fetch('http://mumstudents.org/api/animation',
+            {
+                method: 'GET',
+                headers: {
+                    "content-type": "application/text",
+                    Authorization: `Bearer ${tokenAnim}`
+                }
+            });
+
+        //jsonRes = await response.json()
+        //animationFrame = jsonRes.animationFrame;
+        
+    }
+
     // animationUI(): function loads the animation page
     function animationUI() {
+        let animDisplayArea, refrBtn, lOutBtn;
+
         out.innerHTML = animations;
-        let animDisplayArea = document.querySelector('#animationDisp');
-        let refrBtn = document.querySelector('#refresh');
-        let lOutBtn = document.querySelector('#logout');
+        animDisplayArea = document.querySelector('#animationDisp');
+        refrBtn = document.querySelector('#refresh');
+        refrBtn.addEventListener('click',
+            function () {
+                clearInterval(timers)
+                fetchAnimation()
+            }
+        );
+        lOutBtn = document.querySelector('#logout');
         lOutBtn.addEventListener('click', loggingOut);
 
         // pushes a new state to history
@@ -50,21 +114,21 @@ function main() {
     } // end of animationUI function
 
     // tokenFunction(): function fetches the given URL token
-    function tokenFunction() {
-        fetch(tokUrlAddress, {
+    async function tokenFunction() {
+        let resp, jsonRes, status;
+        resp = await fetch('http://mumstudents.org/api/login', {
             method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            // converts a JavaScript object into string
+            headers: { 'content-type': 'application/json' },
+            // converts a js object to string
             body: JSON.stringify({
                 "username": "mwp",
                 "password": "123"
             })
-        }).then(response => response.json()).then(d => {
-            let tokenAnim = d.token;
-            console.log(tokenAnim);
-        });
+        })
+        jsonRes = await resp.json();
+        tokenAnim = jsonRes.tokenAnim;
+        status = jsonRes.status;
+
     } // end of tokenFunction function
 
     // loggingOut(): function redirects back to logging
@@ -74,6 +138,11 @@ function main() {
         // pushes a new state to history
         history.pushState("loggingOut", "loggingOut History", "/index.html");
     } // end of loggingOut function
+
+
+
+
+
 
     // MyRouters(): function constructor creates two routes for default(LOGIN UI), & animation (ANIMATON UI)
     let MyRouters = function (name, routers) {
