11cdfe5e9cd097216ebc7b13f201605f3f837b2c
Derese92 <54817093+Derese92@users.noreply.github.com>
2019-11-29 01:03:09 -0600

    Third day Progress

--- script.js
+++ script.js
@@ -1,77 +1,139 @@
-window.onload = function(){
-let token;
+window.onload = function () {
 
+    let token;
+    let long;
+    let lati;
+    let keys = "tvXbGrqGqLqjaZ0RV8Bm1Mug02ScMW1T";
 
-const login = `
+    const login = `
 <h1>Please Login</h1><br>
 Username:<input placeholder="mwp" value="mwp"/><br/>
 Password:<input placeholder="123" value="123"/><br/>
 <button id="loginbtn">Login</button>
 `
-let loginTemplate = document.querySelector("#outlet");
-loginTemplate.innerHTML = login;
-let loginId = document.getElementById("loginbtn");
-loginId.addEventListener("click", logToAnimationpage);
+    let loginTemplate = document.querySelector("#outlet");
+    loginTemplate.innerHTML = login;
+    let loginId = document.getElementById("loginbtn");
+    loginId.addEventListener("click", logToAnimationpage);
 
-const animation = `
+    const animation = `
         <div id="address"> Welcome to SPA Animation</div>
         <textarea id="animation" rows="22" cols="22" style="font-size: 20px"></textarea><br><br>
         <button id="refresh">Refresh Animation</button>
         <button id="logout">Logout</button>        
         `
-// let animationTemplate = document.querySelector("#outlet");
-// loginTemplate.innerHTML = animation;
+    // let animationTemplate = document.querySelector("#outlet");
+    // loginTemplate.innerHTML = animation;
 
-function logToAnimationpage() {
-    loginTemplate.innerHTML = animation;
-    logout();
-    refresh();
-}
-// let logoutId = document.getElementById("logout");
-// logoutId.addEventListener("click",logToLoginpage);
-function logout() {
-    document.getElementById("logout").addEventListener("click", logoutto)
-    function logoutto() {
-        loginTemplate.innerHTML = login;
+    function logToAnimationpage() {
+        loginTemplate.innerHTML = animation;
+        logout();
+        refresh();
+        //fetchAddress();
     }
-}
-function refresh() {
-    document.getElementById("refresh").addEventListener("click", refreshfunc)
-    let textarea = document.getElementById("animation");
-    function refreshfunc() {
-        textarea.innerHTML = animFrames;
-
+    // let logoutId = document.getElementById("logout");
+    // logoutId.addEventListener("click",logToLoginpage);
+    function logout() {
+        document.getElementById("logout").addEventListener("click", function() {
+            loginTemplate.innerHTML = login;
+        });
+        
     }
-}
+    function refresh() {
+        document.getElementById("refresh").addEventListener("click", fetchAnim);
+        //let textarea = document.getElementById("animation");
+        // function refreshfunc() {
+        //     textarea.innerHTML = animFrames;
 
-function fetchToken() {
-    fetch("http://mumstudents.org/api/login", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            username: "mwp",
-            password: "123"
-        })
-    })
-        .then(resp => resp.json())
-        .then(data =>{token=data.token;
-            fetchAnim()})
-}
-function fetchAnim(){fetch("http://mumstudents.org/api/animation", {
-                method: 'GET',
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
+        // }
+    }
+
+    function fetchToken() {
+        fetch("http://mumstudents.org/api/login", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                username: "mwp",
+                password: "123"
             })
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                token = data.token;
+                fetchAnim()
+            })
+    }
+    function fetchAnim() {
+        clearInterval(timerId);
+        fetch("http://mumstudents.org/api/animation", {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
             .then(reps => reps.text())
             .then(data => {
-                console.log(data);
-                animFrames = data.split("=====");
-                //console.log(animFrames);
+                // console.log(data);
+                animFrames = data.split("=====\n");
+                // console.log(animFrames);
+                let count = 0;
+                timerId = setInterval(function () {
+                    document.getElementById("animation").innerHTML = animFrames[count];
+                    //console.log(animFrames[count]);
+                    count++;
+                    if (count === animFrames.length) {
+                        //clearInterval(timerId);
+                        count = 0;
+                    }
+                }, 200)
             });
-      
+
+    }
+    fetchToken();
+
+    
+        
+   
+
+    function getLocation() {
+
+        navigator.geolocation.getCurrentPosition(success);
+        function success(position) {
+            console.log('Longitude:' + position.coords.longitude);
+            console.log('Latitude:' + position.coords.latitude);
+
+            // long = position.coords.longitude;
+            // lati = position.coords.latitude;
         }
-fetchToken();
+        // function fail(msg) {
+        //     console.log(msg.code + msg.message); // Log the error
+        // }
+        //console.log(long, lati);
+
+
+    }
+    getLocation();
+
+
+    function fetchAddress() {
+        fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${keys}&location=${41.0123},${-91.97837}`)
+            .then(data => data.json())
+            .then(data =>{ 
+                //let loc = data.results[0].locations[0];
+                console.log(data);
+            
+        let city = data.results[0].locations[0].adminArea5;
+        console.log(city)
+        let state = data.results[0].locations[0].adminArea3;
+        let country = data.results[0].locations[0].adminArea1;
+        let userlocation = `Welcome all from ${city},${state},${country}!`;
+        document.getElementById("address").innerHTML = userlocation;
+        console.log(userlocation)
+            })
+        }
+    fetchAddress();
+
+
 }
