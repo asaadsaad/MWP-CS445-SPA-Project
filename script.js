e3df22217eeb181b3cbff75ca5bf3c29db69e258
semateame <54817239+semateame@users.noreply.github.com>
2019-11-28 16:00:06 -0800

    day 3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let token;
 let timeid;
-let i = 0;
+
+
 
 function SPA() {
     let animation = `<div id="adress">Welcome to SPA Animation</div>
@@ -9,21 +10,23 @@ function SPA() {
 <button id="logout">Logout</button>`
 
     function getlogin() {
-        
+
         let login = `<h2> Please log in </h2>
         user name: <input type="text" id="username" value="mwp"><br>
         Passwword: <input type="text" id="password" value="123"><br>
         <button id="button"> login </button>`
         document.getElementById("outlet").innerHTML = login;
-        
+
     }
     getlogin();
 
     function getanimationpage() {
 
         document.getElementById("outlet").innerHTML = animation
+
         document.getElementById("logout").addEventListener("click", getlogin)
         document.getElementById("refresh").addEventListener("click", gettoken);
+        findaddress()
     }
 
     document.getElementById("button").addEventListener("click", getanimationpage)
@@ -39,17 +42,13 @@ function SPA() {
             })
         let resp = await result.json()
         token = resp
-        
-        
+
+
         final();
-        
+
 
     }
-
-
-
-
-    let final = async function () {
+let final = async function () {
 
         let anime = await fetch("http://mumstudents.org/api/animation",
             {
@@ -60,8 +59,8 @@ function SPA() {
         let response = await anime.text()
         let y = response.split("=====\n")
         clearInterval(timeid);
-        i = 0
-       timeid= setInterval(function () {
+        let i = 0
+        timeid = setInterval(function () {
             document.getElementById("animation").innerHTML = y[i]
             i++
             if (i === y.length) {
@@ -69,26 +68,30 @@ function SPA() {
             }
 
         }, 200)
-
-
-
     }
 
-
-
-
-
-
-
+    function findaddress() {
+        navigator.geolocation.getCurrentPosition(success)
+        async function success(position) {
+            let x = position.coords.longitude;
+            let y = position.coords.latitude;
+            let Address = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=QHd2uOYscxkIZAqAzzwQEWy70b3A8Izg&location=${y},${x}&&includeRoadMetadata=true&includeNearestIntersection=true`)
+            let obj = await Address.json()
+            console.log(obj.results);
+           let city = obj.results[0].locations[0].adminArea5
+           let country =obj.results[0].locations[0].adminArea1;
+       document.getElementById("adress").innerHTML=`Well come to ${city} , ${country}`
+        }
+     }
+    
 }
 SPA()
-
-
-
-
-
 let sema = {
     username: document.getElementById("username").value,
     password: document.getElementById("password").value
 }
+
+
+
+
 
