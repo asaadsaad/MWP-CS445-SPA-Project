3870ad992844fc38d346f431bbc901640d0c5779
mikemum <mwoldemedihin@mum.edu>
2019-11-29 06:23:52 -0600

    refactor and clean up code

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 window.onload = function () {
-    let loginView, loginBtn, animationView, animationArea, refreshBtn, logoutBtn, questApiUrl, lat, long, userLocation, animationToken, animationFrames = [], loadFramesTimeout, displayFramesTimout, displayFrameInterval;
+    let loginView, loginBtn, animationView, animationArea, refreshBtn, logoutBtn, lat, long, userLocation, token, animationFrames = [], displayFrameInterval, i = 0;
+
     const OUTLET = getElement('#outlet');
+    const QUEST_API_URL = 'http://www.mapquestapi.com/geocoding/v1/reverse';
     const QUEST_API_KEY = 'Y0ROZGOJZ8PxsijeatYiupEeXX4y2G4Z';
     const TOKEN_URL = `http://www.mumstudents.org/api/login`;
     const ANIMATION_URL = `http://www.mumstudents.org/api/animation`;
-    let i = 0;
-    
+
     loginView = `
-    <div id="loginView">
-        <h1>Please Login</h1>
-        <label for="username">UserName: </label>
-        <input type="text" id="username" value="mwp"><br>
-        <label for="password">Password: </label> 
-        <input type="password" value="123"><br>
-        <button id="login">Login</button>
-    </div>
-    `;
+        <div id="loginView">
+            <h1>Please Login</h1>
+            <label for="username">UserName: </label>
+            <input type="text" id="username" value="mwp"><br>
+            <label for="password">Password: </label> 
+            <input type="password" value="123"><br>
+            <button id="login">Login</button>
+        </div>
+        `;
 
     animationView = `
-    <div id="animationView">
-        <h2 id="location">Your location</h2>
-        <textarea id="animationArea" cols="100" rows="30"></textarea><br>
-        <button id="refresh">Refresh Animation</button>
-        <button id="logout">Logout</button>
-    </div>
-    `;
+        <div id="animationView">
+            <h2 id="location">Your location</h2>
+            <textarea id="animationArea" cols="100" rows="30"></textarea><br>
+            <button id="refresh">Refresh Animation</button>
+            <button id="logout">Logout</button>
+        </div>
+        `;
     getLatLong();
     loadLogin();
 
@@ -47,7 +48,30 @@ window.onload = function () {
         OUTLET.innerHTML = loginView;
         loginBtn = getElement('#login');
         loginBtn.addEventListener('click', loadAnimationView);
-        
+
+        let loginViewState = `
+            <div id="loginView">
+                <h1>Please Login</h1>
+                <label for="username">UserName: </label>
+                <input type="text" id="username" value="mwp"><br>
+                <label for="password">Password: </label> 
+                <input type="password" value="123"><br>
+                <button id="login" onclick="loadAnimationView()">Login</button>
+            </div>
+            `;
+    }
+
+    /**
+   * Function to get user location asynchronously
+   */
+    function getUserLocation() {
+        mapQuestApiUrl = `${QUEST_API_URL}?key=${QUEST_API_KEY}&location=${lat},${long}`;
+
+        fetch(mapQuestApiUrl).then(response => response.json()).then(data => {
+            let address = data.results[0].locations[0];
+            userLocation = `${address.adminArea5}, ${address.adminArea3}, ${address.adminArea1}`;
+            getElement('#location').innerHTML = `Wellcome all from ${userLocation}`;
+        })
     }
 
     /**
@@ -55,7 +79,6 @@ window.onload = function () {
      */
     function loadAnimationView() {
         getUserLocation();
-
         OUTLET.innerHTML = animationView;
         animationArea = getElement('#animationArea');
 
@@ -65,79 +88,65 @@ window.onload = function () {
         logoutBtn = getElement('#logout');
         logoutBtn.addEventListener('click', logOut);
 
-        getToken();
-
-        loadFramesTimeout = setTimeout(() => {
-            getAnimationFrames();
-        }, 1000)
-
-        displayFramesTimout = setTimeout(() => {
-            displayAnimation();
-        }, 1500);
+        clearInterval(displayFrameInterval);
+        getAnimationFrames();
     }
 
     /**
-     * Function to get user location asynchronously
-     */
-    function getUserLocation() {
-        //41.0031878,-91.965341
-        mapQuestApiUrl = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${QUEST_API_KEY}&location=${lat},${long}`;
-        console.log(mapQuestApiUrl);
-
-        fetch(mapQuestApiUrl).then(response => response.json()).then(data => {
-            let address = data.results[0].locations[0];
-            userLocation = `${address.street}, ${address.adminArea5}, ${address.adminArea3}, ${address.postalCode}`;
-            getElement('#location').innerHTML = userLocation;
-        })
-    }
-
-    /**
-     * Function to get token for animation api using POST request asynchronously
-     */
-    function getToken() {
-        fetch(TOKEN_URL, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                "username": "mwp",
-                "password": "123"
-            })
-        }).then(response => response.json()).then(data => {
-            animationToken = data.token;
-            console.log(animationToken);
-        });
-    }
-
-    /**
-     * Function to get animation frames from the animation api using GET request asynchronously
-     */
+   * Function to get animation frames from the animation api using GET request asynchronously
+   */
     function getAnimationFrames() {
-        console.log(animationToken);
-        fetch(ANIMATION_URL, {
+        if (!token) {
+            fetch(TOKEN_URL, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "username": "mwp",
+                    "password": "123"
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    token = data.token;
+                    console.log('token: ' + token);
+                    fetchFrames(ANIMATION_URL);
+                });
+        } else {
+            console.log('token: ' + token);
+            fetchFrames(ANIMATION_URL);
+        }
+    }
+
+    /**
+     * Function to fetch animation frames asynchronously
+     * @param {String} url animation frame url
+     */
+    function fetchFrames(url) {
+        fetch(url, {
             method: 'GET',
             headers: {
-                'Authorization': `Bearer ${animationToken}`
+                'Authorization': `Bearer ${token}`
             }
-        }).then(response => response.text()).then(data => {
-            console.log(data);
-            animationFrames = data.split('=====');
-        });
+        })
+            .then(response => response.text())
+            .then(data => {
+                animationFrames = data.split('=====');
+                displayAnimation();
+            });
     }
 
     /**
      * Function to display animation frames
      */
     function displayAnimation() {
-        debugger
         displayFrameInterval = setInterval(() => {
             if (i < animationFrames.length) {
-                animationArea.value = animationFrames[i++]; 
+                animationArea.value = animationFrames[i++];
             } else {
                 i = 0;
             }
-            // i++;
         }, 200);
     }
 
@@ -145,24 +154,18 @@ window.onload = function () {
      * Function to refresh the animation view asynchronously
      */
     function refreshAnimation() {
-        clearTimeout(loadFramesTimeout);
-        clearTimeout(displayFramesTimout);
         clearInterval(displayFrameInterval);
         i = 0;
 
-        loadFramesTimeout = setTimeout(() => {
-            getAnimationFrames();
-        }, 1000)
-
-        displayFramesTimout = setTimeout(() => {
-            displayAnimation();
-        }, 1500);
+        getAnimationFrames();
     }
 
     /**
      * Function to logout and redirect to the login view
      */
     function logOut() {
+        clearInterval(displayFrameInterval);
+        i = 0;
         loadLogin();
     }
 
@@ -174,30 +177,3 @@ window.onload = function () {
         return document.querySelector(selector);
     }
 }
-
-/**
- * inject the login view
- * add click event to the login button
- * upon clicking the login button do the following:
- * 1. inject animation view
- *      - add click event to refresh animation and logout           buttons
- * 2. make a get request to geolocation api:
- *      -get the latitude and longitued from the                  response
- * 3. use the latitude and longitude to make a get request to mapquest api:
- *      -get the information you need street, city, zip          from the response and
- *      -display it on animation header
- * 4. make a post request to http://www.mumstudents.org/api/login to get token
- * 5. use this token to make a GET request to http://www.mumstudents.org/api/animation to get the animation text
- * 6. extract the animation text from the response
- * 7. split the animation text using '====='
- * 8. display each frame in the animation text area every 200 millisecond
- * 9. when the use clicks the refresh button:
- *      - make a new GET request to http://www.mumstudents.org/api/animation using the same token
- *      -extract,split,display (the same thing like in step 6,7,8 except this time the frames are different)
- *      - puhs the current page to the history
- *      - clear intervals that you used earlier to avoid unexpected output
- *10. when the user clicks the logout page:
-        - push the current page to the history
-        -load login page
-        -change the route to be displayed
- */
