7ad5827f59fcaec2d040ae39bd2420a23d87b6a4
Tariku Gelan <tgelan@mum.edu>
2019-11-28 23:33:04 -0600

    day 3 commit, daily progress is on Readme file if you care to read

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ today i have tried to work on fetching the token and fetching the
 animation and i was able to display the animation on the text area
 i am planing to do the geolcation functionality and the rest for thursday, on thanks giving day....
 i will email all the problems i have faced during fetching the animation
+
+today 11/28/19
+today i was working on geolocation api and i was able to fetch my location using map quest api, it was succesfully displayed on the h3 header. i signed up for the service to get the developer key. i was having difficulty to assign the lattitude and longitude by using navigator, i will s finish up the animation totaly tomoorrow anad i am planned to history api pushing tommorrow
--- script.js
+++ script.js
@@ -3,13 +3,19 @@
 /* eslint-disable quotes */
 /* eslint-disable strict */
 window.onload = function () {
-    //let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBd…w8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA"
+
+    // navigator.geolocation.getCurrentPosition(function (position) {
+    //     console.log(position);
+    // });
+    
+        let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBd…w8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA"
     const loginPage = `<h1> Please login</h1>
-Username <input type="text" placeholder="mwp" value = "mwp"/> <br>
-Password <input type="text" placeholder="123" value = "123"/> <br>
-<button id ="logInButton">Login</button> <br>`;
+    Username <input type="text" placeholder="mwp" value = "mwp"/> <br>
+    Password <input type="text" placeholder="123" value = "123"/> <br>
+   <button id ="logInButton">Login</button> <br>`;
     const animationPage = `
-        <textarea name=" Welcome all from " id= "animTextArea" cols="60" rows="20"></textarea><br>
+        <h3 id = "location"></h3>
+        <textarea id= "animTextArea" cols="60" rows="20"></textarea><br>
         <button id ="Refanimation">Refresh Animation</button> 
         <button id ="logoutid">Logout</button> <br>`;
 
@@ -21,8 +27,10 @@ Password <input type="text" placeholder="123" value = "123"/> <br>
 
     function nextPage() {
         outlet.innerHTML = animationPage;
+        yourLocation()
         redirectingPage();
         refFunction()
+
         //fetchForAnim()    //to load the second page
     }
     function redirectingPage() {
@@ -34,35 +42,56 @@ Password <input type="text" placeholder="123" value = "123"/> <br>
     }
 
     function refFunction() {
-        let txtarAnimPage = document.getElementById("animTextArea");
+        // let txtarAnimPage = document.getElementById("animTextArea");
         let refButn = document.getElementById("Refanimation");
 
-        refButn.addEventListener("click", function () {
-            txtarAnimPage.innerHTML = displayAnim;
-        })
+        refButn.addEventListener("click",moveAnim)
 
     }
     function fetcForToken() {
         fetch("http://mumstudents.org/api/login",
             {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
+                headers: {'Content-Type': 'application/json' },
                 body: JSON.stringify({ username: "mwp", password: "123" })
             })
             .then(res => res.json())
-            .then(elem => {
+            .then(elem => {token = elem.token
+                moveAnim()}
+            )}
+            
+    function moveAnim(){   
+        fetcForToken();        
                 fetch("http://mumstudents.org/api/animation", {
                     method: 'GET',
-                    headers: { "Authorization": `Bearer ${elem.token}` },
+                    headers: { "Authorization": `Bearer ${token}` },
 
                 })
                     .then(res => res.text())
                     .then(elem => {
-                        displayAnim = elem.split("=====")
-                        console.log(elem)
+                      let displayArray = elem.split("=====/n")
+                        console.log(displayArray)
+                        document.getElementById("animTextArea").innerHTML = displayArray
                     })
-            })
+        
     }
-    fetcForToken();
+    
+
+    function yourLocation() {
+        fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=
+               UGe5a24nOhIAkbWIG3UCWtSDN0xKJeZ3&location=41.0076,-91.9637`)
+            .then((res) => res.json())
+            .then((data) => {
+                let Mylocation = data.results[0].locations[0];
+                let state = Mylocation.adminArea5;
+                let city = Mylocation.adminArea3
+                let country = Mylocation.adminArea1
+                document.getElementById("location").innerHTML = `Welcome to all from  ${state} ${city},${country}`
+
+            });
+    }
 
 }
+
+
+
