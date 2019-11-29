f82df3d62a5088e0faa5d111abdd4483b0948366
nLeul <leulnecha@gmail.com>
2019-11-29 00:24:44 -0600

    fetched position and displayed into the text area

--- script.js
+++ script.js
@@ -1,51 +1,34 @@
 // your code here
 window.onload = function () {
-    // let token = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtd2EiLCJpc3N1ZWRBdCI6IjIwMTktMTEtMjciLCJ1c2VybmFtZSI6Im13cCJ9.U9ciwh5lcPwFlJdxhNQkeiMc7AMYAnawfKNidw8CNDpTIUjNBL_EtDqkXG4qGOF8H_Ve1S2Gg2PwmCYOkfgmWA`;
+    let mapsKey = `CSFyNjI9A2gLtHH9xNLjS7oOQGpjaHED`;
     let loginPage =
         `<h3>Please Login</h3>Username <input id="username" type="text"><br>
 Password <input id="password" type="text"><br>
 <button id="loginId">Login</button>`;
 
-    let animationPage = `    <h3>welcome All from<h3 id="location"></h3>
+    let animationPage = `    <h3 id="location">welcome All from <h3 ></h3>
                 </h3>
                 <textarea id="textarea" cols="50" rows="15"></textarea><br>
                 <button id="refAnimBtn">Refresh Animation</button>
                 <button id="logoutButn">Logout</button>`;
-
-
     let outlet = document.getElementById("outlet");
     outlet.innerHTML = loginPage;
-
     let loginBtn = document.getElementById("loginId");
     loginBtn.addEventListener("click", loadAnimationPage);
-
     function loadAnimationPage() {
         outlet.innerHTML = animationPage;
         logOut();
         refresh();
+        getPos();
     }
-
     function logOut() {
-
         document.getElementById("logoutButn").addEventListener("click", logOutFunc);
         function logOutFunc() {
-
             outlet.innerHTML = loginPage;
         }
     }
-    function refresh() {
-        document.getElementById("refAnimBtn").addEventListener("click", refreshFunc);
-        let textNode = document.getElementById("textarea");
-        function refreshFunc() {
-
-            textNode.innerHTML = animate(d);
-        }
-    }
     async function getToken() {
-        // let user = document.getElementById()
-        // let password = document.getElementById();
-
-        let response = await fetch("http://mumstudents.org/api/login", {
+        let tokenResponse = await fetch("http://mumstudents.org/api/login", {
             method: "POST",
             headers: {
                 "Accept": "application/json,text/plain,*/*",
@@ -57,56 +40,50 @@ Password <input id="password" type="text"><br>
                     "password": "123"
                 })
         })
-        let data = await response.json();
-        // let output=await data.json()
-        // token = output.token;
-        // token = data.token;
-        // console.log(token);
-
-
-        let a = await fetch("http://www.mumstudents.org/api/animation", {
+        let tokenObj = await tokenResponse.json();
+        let animeResponse = await fetch("http://www.mumstudents.org/api/animation", {
             method: "GET",
-            headers: { "Authorization": `Bearer ${data.token}` }
+            headers: { "Authorization": `Bearer ${tokenObj.token}` }
         });
-        let b = await a.text()
-        let c = await b.split('=====');
-         d = await animate;
-        console.log(d);
-       
-     
-    
-
-       // .then(data => data.text())
-        // .then(elem => console.log(elem));
-        // console.log(elem);
-
-
+        let animeString = await animeResponse.text();
+        // console.log(animeString);
+        eachString = animeString.split('=====');
+        // console.log(eachString);
+        function animate(arr) {
+            for (let i = 0; i < arr.length; i++){
+                setInterval(() => {
+                   console.log(arr[i]) 
+                }, 500);
+            }
+        }
+        animate(eachString);
     }
     getToken();
-     function animate(arr) {
-   arr.forEach((element,index,arra )=> {
-       setInterval((element) => {
-           console.log(arra[index])
-       }, 500);
-   });
-    } 
+    function refresh() {
+        document.getElementById("refAnimBtn").addEventListener("click", refreshFunc);
+        let textNode = document.getElementById("textarea");
+        function refreshFunc() {
 
-
-
+            textNode.innerHTML = eachString;
+        }
+    }
+    async function getPos() {
+        let position = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=${mapsKey}&location=30.333472,-81.470448`)
+        let obj = await position.json()
+        let location = obj.results[0].locations;
+        document.getElementById("location").innerHTML = `Welcome to ${location[0].adminArea5},${location[0].adminArea3},${location[0].adminArea1}`;
+    }
+    getPos();
 }
-// async function getAnime() {
-//     let response = await fetch("http://www.mumstudents.org/api/animation", {
-//         method: "GET",
-//         headers: {
-//             "Authorization": `Bearer ${token}`
-//         }
-//     });
-//     let obj = await response.text();
+            // arr.forEach((element, index, arra) => {
+            //     setInterval((element) => {
+            //         console.log(arra[index])
+            //     }, 500);
+            // });
 
-//     console.log(typeof (obj));
-//     console.log( obj);
 
-// }
 
-// "Accept": "application/json,text/plain,*/*",
+
+
+
 
