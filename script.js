ea4da9f53fa4ed80061ddc86ba6d36d366d9423d
semateame <54817239+semateame@users.noreply.github.com>
2019-11-30 09:26:50 -0800

    day 4

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-let token;
-let timeid;
-
-
 
 function SPA() {
-    let animation = `<div id="adress">Welcome to SPA Animation</div>
-<textarea name="" id="animation" cols="50" rows="20" style="font-size: 18px;"></textarea> <br>
-<button id="refresh">Refresh Animation</button>
-<button id="logout">Logout</button>`
+    let token;
+    let timeid;
+    let login = `<h2> Please log in </h2>
+    user name: <input type="text" id="username" value="mwp"><br>
+    Passwword: <input type="text" id="password" value="123"><br>
+    <button id="button"> login </button>`
 
-    function getlogin() {
+    let animation = `
+    <div id="adress">Welcome to SPA Animation</div>
+    <textarea name="" id="animation" cols="50" rows="20" style="font-size: 18px;"></textarea> <br>
+    <button id="refresh">Refresh Animation</button>
+    <button id="logout">Logout</button>`
 
-        let login = `<h2> Please log in </h2>
-        user name: <input type="text" id="username" value="mwp"><br>
-        Passwword: <input type="text" id="password" value="123"><br>
-        <button id="button"> login </button>`
+    async function getlogin() {
+
         document.getElementById("outlet").innerHTML = login;
-
-    }
-    getlogin();
-
-    function getanimationpage() {
-
-        document.getElementById("outlet").innerHTML = animation
-
-        document.getElementById("logout").addEventListener("click", getlogin)
-        document.getElementById("refresh").addEventListener("click", gettoken);
-        findaddress()
-    }
-
-    document.getElementById("button").addEventListener("click", getanimationpage)
-
-
-    let gettoken = async function () {
+        clearInterval(timeid);  
+        let sema = {
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        }
 
         let result = await fetch("http://mumstudents.org/api/login",
             {
@@ -42,14 +30,43 @@ function SPA() {
             })
         let resp = await result.json()
         token = resp
-
-
-        final();
-
-
+        document.getElementById("button").addEventListener("click", animationpage)
+       // document.getElementById("button").addEventListener('click', _ => history.pushState({ page: 1 }, "index.html", "?login"))
     }
-let final = async function () {
+    getlogin();
 
+    function animationpage() {
+
+        
+        document.getElementById("outlet").innerHTML = animation
+      //  document.getElementById("refresh").addEventListener('click', _ => history.pushState({ page: 2 }, "index.html", "?animation"))
+
+        document.getElementById("logout").addEventListener("click", getlogin)
+        document.getElementById("refresh").addEventListener("click", getAnimation);
+        findaddress()
+    }
+
+    
+
+
+    // let gettoken = async function () {
+
+    //     let result = await fetch("http://mumstudents.org/api/login",
+    //         {
+    //             method: "POST",
+    //             headers: { "Content-Type": "application/json" },
+    //             body: JSON.stringify(sema)
+    //         })
+    //     let resp = await result.json()
+    //     token = resp
+
+
+    //     final();
+
+
+    // }
+    let getAnimation = async function () {
+   
         let anime = await fetch("http://mumstudents.org/api/animation",
             {
                 headers: {
@@ -61,13 +78,16 @@ let final = async function () {
         clearInterval(timeid);
         let i = 0
         timeid = setInterval(function () {
+            
             document.getElementById("animation").innerHTML = y[i]
+            //console.log(y[0])
             i++
             if (i === y.length) {
                 i = 0
             }
 
         }, 200)
+
     }
 
     function findaddress() {
@@ -78,18 +98,16 @@ let final = async function () {
             let Address = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=QHd2uOYscxkIZAqAzzwQEWy70b3A8Izg&location=${y},${x}&&includeRoadMetadata=true&includeNearestIntersection=true`)
             let obj = await Address.json()
             console.log(obj.results);
-           let city = obj.results[0].locations[0].adminArea5
-           let country =obj.results[0].locations[0].adminArea1;
-       document.getElementById("adress").innerHTML=`Well come to ${city} , ${country}`
+            let city = obj.results[0].locations[0].adminArea5
+            let country = obj.results[0].locations[0].adminArea1;
+            document.getElementById("adress").innerHTML = `Well come to ${city} , ${country}`
         }
-     }
-    
+    }
+
 }
+
 SPA()
-let sema = {
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value
-}
+
 
 
 
