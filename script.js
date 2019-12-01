c6f84ad20ee5943047c143d5e42a48a9898d81b7
mikemum <mwoldemedihin@mum.edu>
2019-12-01 16:33:50 -0600

    refactor code and add loading gif

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-window.onload = function () {
-    let loginView, animationView, animationArea, lat, long, userLocation, token, animationFrames = [], displayFrameInterval, i = 0, state, animationCounter = 0;
+'use strict';
+
+window.onload = () => {
+    let animationArea, lat, long, userLocation, token, animationFrames = [], animationInterval, i = 0, state, frameId = 0;
 
     const OUTLET = getElement('#outlet');
     const QUEST_API_URL = 'http://www.mapquestapi.com/geocoding/v1/reverse';
     const QUEST_API_KEY = 'Y0ROZGOJZ8PxsijeatYiupEeXX4y2G4Z';
     const TOKEN_URL = `http://www.mumstudents.org/api/login`;
     const ANIMATION_URL = `http://www.mumstudents.org/api/animation`;
-
-    loginView = `
+    const VIEWS = {
+        loginView: `
         <div id="loginView">
             <h1>Please Login</h1>
             <label for="username">UserName: </label>
@@ -16,46 +18,42 @@ window.onload = function () {
             <input type="password" value="123"><br>
             <button id="login">Login</button>
         </div>
-        `;
-
-    animationView = `
+        `,
+        animationView: `
         <div id="animationView">
             <h2 id="location">Your location</h2>
             <textarea id="animationArea" cols="100" rows="30"></textarea><br>
             <button id="refresh">Refresh Animation</button>
             <button id="logout">Logout</button>
         </div>
-        `;
-
+        `
+    }
     const ROUTES = {
-        '/': loginView,
-        '/login': loginView,
-        '/animation': animationView
+        '/': VIEWS.loginView,
+        '/login': VIEWS.loginView,
+        '/animation': VIEWS.animationView
     };
 
     window.addEventListener('popstate', popstateHandler);
 
-    initialize();
-
     /**
-     * Function to load the initial view
+     * Self invoking arrow function to load the initial view
      */
-    function initialize() {
+    (() => {
         getLatLong();
-        state = { data: null, path: '/', userLocation: userLocation, initialState: true };
+        state = { frames: null, path: '/', userLocation: userLocation };
         history.replaceState(state, null, '/login');
         render(state);
-    }
+    })();
 
     /**
      * Function to render the state
      * @param {Object} state state to the current view
      */
     function render(state) {
-        debugger
         OUTLET.innerHTML = ROUTES[state.path];
         let btns = document.querySelectorAll('button');
-        console.log(btns);
+
         if (btns) {
             btns.forEach((btn) => {
                 if (btn.id === 'refresh') {
@@ -71,11 +69,7 @@ window.onload = function () {
         if (location) {
             location.innerHTML = `Wellcome all from ${state.userLocation}`;
         }
-        if (state.data) {
-            displayAnimation(state.data);
-        } else {
-            displayAnimation(animationFrames);
-        }
+        animate(state.frames);
     }
 
     /**
@@ -90,7 +84,7 @@ window.onload = function () {
     }
 
     /**
-     * Function to get the current user lat and long
+     * Function to get the current user latitude and longitude
      */
     function getLatLong() {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -104,26 +98,28 @@ window.onload = function () {
    * Function to get user location asynchronously
    */
     async function getLocationAsync() {
-        mapQuestApiUrl = `${QUEST_API_URL}?key=${QUEST_API_KEY}&location=${lat},${long}`;
+        let mapQuestApiUrl = `${QUEST_API_URL}?key=${QUEST_API_KEY}&location=${lat},${long}`;
 
         let response = await fetch(mapQuestApiUrl);
         return response.json();
     }
 
     /**
-     * Function to load animation view
+     * Function to login and load animation asynchronously
      */
     async function logInAsync() {
+        OUTLET.innerHTML = `<img src="loading.gif" alt="Loading gif">`;
+
         let address = await getLocationAsync();
         address = address.results[0].locations[0];
         userLocation = `${address.adminArea5}, ${address.adminArea3}, ${address.adminArea1}`;
 
         await fetchFrameAsync();
-        state.initialState = false;
-        state.data = animationFrames;
+
+        state.frames = animationFrames;
         state.path = '/animation';
         state.userLocation = userLocation;
-        history.pushState(state, null, `/animation?${animationCounter++}`);
+        history.pushState(state, null, `/animation?${frameId++}`);
         render(state);
     }
 
@@ -145,14 +141,13 @@ window.onload = function () {
         response = await response.json();
         token = response.token;
     }
-    
+
     /**
      * Function to fetch animation frames asynchronously
-     *
      */
     async function fetchFrameAsync() {
         if (!token) {
-            await getTokenAsync(); 
+            await getTokenAsync();
         }
         let response = await fetch(ANIMATION_URL, {
             method: 'GET',
@@ -167,13 +162,15 @@ window.onload = function () {
     /**
      * Function to display animation frames
      */
-    function displayAnimation(frames) {
-        clearInterval(displayFrameInterval);
+    function animate(frames) {
+        if (!frames) {
+            return;
+        }
+        clearInterval(animationInterval);
         animationArea = getElement('#animationArea');
-        console.log(frames.join('====='));
 
         if (frames.length > 0) {
-            displayFrameInterval = setInterval(() => {
+            animationInterval = setInterval(() => {
                 if (i < frames.length) {
                     animationArea.value = frames[i++];
                 } else {
@@ -187,13 +184,14 @@ window.onload = function () {
      * Function to refresh the animation view asynchronously
      */
     async function refreshAsync() {
+        OUTLET.innerHTML = `<img src="loading.gif" alt="Loading gif">`;
         i = 0;
         await fetchFrameAsync();
-        state.initialState = false;
-        state.data = animationFrames;
+
+        state.frames = animationFrames;
         state.path = '/animation';
         state.userLocation = userLocation;
-        history.pushState(state, null, `/animation?${animationCounter++}`);
+        history.pushState(state, null, `/animation?${frameId++}`);
         render(state);
     }
 
@@ -201,10 +199,10 @@ window.onload = function () {
      * Function to logout and redirect to the login view
      */
     function logOut() {
-        clearInterval(displayFrameInterval);
+        clearInterval(animationInterval);
         i = 0;
         animationFrames = [];
-        state.data = animationFrames;
+        state.frames = animationFrames;
         state.path = '/';
         history.pushState(state, null, `/login`);
         render(state);
