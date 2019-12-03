bd6cf89e08e290175d8ef013df02a1b66d8c7f04
sophia-blitz <sblitz@mum.edu>
2019-12-03 16:59:38 -0600

    add loading display, all done but wrap in function

--- script.js
+++ script.js
@@ -26,18 +26,24 @@ let outlet;
 const logInPageTemplate = `
   <div class="container" id="login-div">
   <h1>Please login</h1>
-    <div id="login-error" class="hidden">Invalid username/password. <br>Try again. </div>
-    Username: <input type="text" id="username" value="mwp"> <br>
-    Password: <input type="password" id="password" value="123"> <br>
+    <div id="login-error" class="hidden error">Invalid username/password. <br>Try again. </div>
+    Username: <input type="text" id="username" value=""> <br>
+    Password: <input type="password" id="password" value=""> <br>
     <button id="login-btn">Log in</button> 
+    <div id="connection-error" class="error">
+      Connection Error. Please try again.
+    </div>
   </div>
-`; // TODO remove default log in data
+`; 
 const animationPageTemplate = `
   <div class="container" id="animation-div">
     Welcome all from <span id="location-span">somewhere</span> <br>
     <textarea id="animation-area"> </textarea> <br>
     <button id="refresh-btn">Refresh</button>
     <button id="logout-btn">Log out</button>
+    <div id="connection-error" class="error">
+      Connection Error. Please try again.
+    </div>
   </div>
 `;
 
@@ -49,6 +55,18 @@ function processAndDisplayAnimation(rawText) {
   clearInterval(animationTimerId);
   animationIndex = 0;
   animationTimerId = setInterval(animate, 200);
+}
+
+function clearSessionData(){
+  // cancel interval
+  clearInterval(animationTimerId);
+
+  // clear app session data
+  animationTimerId = null;
+  token = null;
+  animationFrames = null;
+  animationIndex = 0;
+  isLoggedIn = false;
 }
 
 
@@ -86,17 +104,16 @@ You will need to pass an API key and the user current coordinates as query param
 This request should return back a JSON object with full details about the location.
  */
 async function fetchLocation(long, lat, success, fail) {
-  
   const key = "94NZs8hqHEZqPbNAztQlsonqALhJJ1Ic";
   try {
     const response = await fetch(`https://www.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${lat}%2C${long}&outFormat=json&thumbMaps=false`);
     const bodyObj = await response.json();
     const locations = bodyObj.results[0].locations;
-    if (locations.length>0){
+    if (locations.length > 0) {
       let location = locations[0].adminArea5 + ", " + locations[0].adminArea3 + ", " + locations[0].adminArea1;
       success(location);
     }
-   
+
 
   } catch (error) {
     console.error(error);
@@ -110,8 +127,8 @@ Request body: JSON format
 {“username”: “mwp”, “password”: “123”
 The server will send a JSON response with the following format: {token: string, status: true}
  */
-async function fetchToken(user, pw, success, fail) {
-
+async function fetchToken(user, pw, withAccess, noAccess, connectionError) {
+  showLoading();
   try {
     const response = await fetch("http://mumstudents.org/api/login", {
       method: "POST",
@@ -120,13 +137,18 @@ async function fetchToken(user, pw, success, fail) {
     });
     const bodyObj = await response.json();
     token = bodyObj.token;
-    success();
+    if (token) {
+      withAccess()
+    } else {
+      noAccess();
+    }
+
   } catch (error) {
     console.error(error);
-    fail();
+    connectionError();
   }
 
-
+  hideLoading();
 }
 
 
@@ -136,14 +158,23 @@ async function fetchToken(user, pw, success, fail) {
 function displayLoginError() {
   document.getElementById("login-error").classList.remove("hidden");
 }
+function displayConnectionError() {
+  errorDiv = document.getElementById("connection-error");
+  errorDiv.classList.add("show");
+  setTimeout(_ => {
+    errorDiv = document.getElementById("connection-error").classList.remove("show");
+  }, 5000);
+}
 function displayAnimationError() {
   document.getElementById("animation-area").value = "Error: Could not load animation";
 }
 function displayLocationError() {
-  document.getElementById("location-span").innerText = "Earth";
+  let el = document.getElementById("location-span");
+  if(el){el.innerText = "Earth";}
 }
 function displayLocation(location) {
-  document.getElementById("location-span").innerText = location;
+  let el = document.getElementById("location-span");
+  if (el) { el.innerText = location; }
 }
 function animate() {
   animationIndex = (++animationIndex) % animationFrames.length;
@@ -152,6 +183,7 @@ function animate() {
 function goToLogin() {
   // change state
   history.pushState({ page: 1 }, "Login", "?page=1");
+  isLoggedIn = true;
 
   // set html
   outlet.innerHTML = logInPageTemplate;
@@ -185,6 +217,12 @@ function goToAnimation() {
   // request animation
   fetchAnimation(processAndDisplayAnimation, displayAnimationError);
 }
+function showLoading() {
+  document.getElementById("loading").classList.remove("hidden");
+}
+function hideLoading() {
+  document.getElementById("loading").classList.add("hidden");
+}
 
 
 /****************************
@@ -194,31 +232,30 @@ function onload() {
   outlet = document.getElementById("outlet");
   goToLogin();
 }
-function onpopstate() {
-  // TODO
+function onpopstate(event) {
+  if (event.state.page == 1) {
+    clearSessionData()
+    goToLogin();
+  }
+
 }
 function onClickLogin() {
+
   // get username/pw from user
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  // validate user / fetch access token. on success load animation page. on fail, display error
-  fetchToken(username, password, goToAnimation, displayLoginError);
+  // validate user 
+  // on success, load animation page. 
+  // with no access, display log in error
+  // on connectino error, display error
+  fetchToken(username, password, goToAnimation, displayLoginError, displayConnectionError);
 }
 function onClickLogout() {
-  // cancel interval
-  clearInterval(animationTimerId);
-
-  // clear app session data
-  animationTimerId = null;
-  token = null;
-  animationFrames = null;
-  animationIndex = 0;
-  isLoggedIn = false;
+  clearSessionData()
 
   // load log in page
   goToLogin();
-
 }
 function onClickRefreshAnimation() {
   // request animation
