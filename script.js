28dc260a6dd2fb752dbd0fee1fbc3e4193d41118
tahirmama351 <tahirmama@gmail.com>
2020-12-13 14:20:12 -0600

    Building template

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // your code here
+window.onload = function () {
+
+     let loginTemplate = `
+     <div >
+     <p style="font-size: 30px"> <b> Login Here </p>
+     
+     User Name: <input  id="username" type="text"  placeholder="tahir" value="tahir"> <br>
+
+     Password: <input id="password" type="password"  placeholder="123456" value="123456"> <br><br>
+
+     <input  id="login" type="button" value="Login"> 
+     </div>`
+
+     document.getElementById("outlet").innerHTML = loginTemplate;
+
+     function displayAnimaTemplate() {
+          let animationTemplate = `
+     <div style="background-color:LightGray">
+     <p >Welcome  to Fair Fild</p>
+     <textarea  id="animationArea"></textarea> <br>
+     <input type="button"  id="refreshAnimation" value="Refresh Animation">
+     <input type="button" id="logoutBotton" value="Logout"  >
+     </div>`
+          document.getElementById("outlet").innerHTML = animationTemplate;
+     }
+
+     document.getElementById("login").addEventListener("click", displayAnimaTemplate);
+}
+
+
+
+
+
