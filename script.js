324a37f0c4f6590266f5a76a10ca6103f56310dc
Hung Le <dangky1410@gmail.com>
2020-12-12 21:09:34 -0600

    building animation page

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 window.onload = function () {
+  const outlet = document.getElementById("outlet");
+
   setTimeout(() => {
-    const outlet = document.getElementById("outlet");
     outlet.innerHTML = `<h1>Please Login</h1>
-Username <input><br>
-Password <input><br>
-<button>login</button`;
+Username <input value = "hungle"><br>
+Password <input value = "123456"><br>
+<button onclick="login()">Login</button`;
   }, 1000);
 };
+
+function login() {
+
+  outlet.innerHTML = `<h3>Welcome</h3>
+ <textarea rows="10" cols="50"></textarea><br>
+
+
+
+
+
+  <button onclick="refresh()">Refresh Animation</button>
+  <button onclick="logout()">Logout</button>
+  `;
+}
