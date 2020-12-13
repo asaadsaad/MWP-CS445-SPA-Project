f098c817c3a5a015efc87a0a8f0b345047568019
Hung Le <dangky1410@gmail.com>
2020-12-13 15:53:32 -0600

    display location on animation page added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 window.onload = function () {
   const outlet = document.getElementById("outlet");
 
@@ -10,15 +11,30 @@ Password <input value = "123456"><br>
 };
 
 function login() {
-
-  outlet.innerHTML = `<h3>Welcome</h3>
+  outlet.innerHTML = `<h3>Welcome all from <span id="location"></span></h3>
  <textarea rows="10" cols="50"></textarea><br>
-
-
-
 
 
   <button onclick="refresh()">Refresh Animation</button>
   <button onclick="logout()">Logout</button>
   `;
+
+  let geoKey = "hl5CFvCH0GGgkLuulKNPcQ5q1bPDGvYH";
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    let geoLink = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${geoKey}&location=${lat},${long}&includeRoadMetadata=true&includeNearestIntersection=true`;
+
+    fetch(geoLink)
+      .then((r) => r.json())
+      .then((data) => {
+        let res = data.results[0].locations[0];
+        console.log(`${res.adminArea5}, ${res.adminArea3} `);
+
+        let place = document.getElementById("location");
+
+        place.innerHTML = `${res.adminArea5}, ${res.adminArea3} `;
+      });
+  });
 }
