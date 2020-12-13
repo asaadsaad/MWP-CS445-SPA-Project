73854eabea4c37b17e7b02e7f9cb496c4fa714c1
Hung Le <dangky1410@gmail.com>
2020-12-12 20:40:41 -0600

    building html

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// your code here
+window.onload = function () {
+  setTimeout(() => {
+    const outlet = document.getElementById("outlet");
+    outlet.innerHTML = `<h1>Please Login</h1>
+Username <input><br>
+Password <input><br>
+<button>login</button`;
+  }, 1000);
+};
