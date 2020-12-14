3cb6c5bbf8256cb38e76b11d5009ca731ff3c5b0
Hung Le <dangky1410@gmail.com>
2020-12-14 16:13:08 -0600

    replace onclick with addeventlistener

--- script.js
+++ script.js
@@ -5,93 +5,102 @@ let timerId;
 window.onload = function () {
   outlet = document.getElementById("outlet");
 
-  window.loginTemplate = `
+  const loginTemplate = `
   <h1>Please Login</h1>
   Username <input value = "mwp"><br>
   Password <input value = "123"><br>
-  <button onclick="login()">Login</button
+  <button id="login">Login</button
 `;
-  outlet.innerHTML = window.loginTemplate;
-  history.pushState({ page: 1 }, null, "?login");
-};
-
-async function login() {
-  history.pushState({ page: 2 }, null, "?animation");
-
-  const response = await fetch(
-    "https://cs445-project.herokuapp.com/api/login",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: "map",
-
-        password: "123456",
-      }),
-    }
-  );
-
-  const result = await response.json();
-  loginToken = result;
-  console.log(loginToken);
   const animationTemplate = `<h3>Welcome all from <span id="location"></span></h3>
  <textarea id="animation" rows="10" cols="50"></textarea><br>
 
-  <button onclick="refresh()">Refresh Animation</button>
-  <button onclick="logout()">Logout</button>
+  <button id="refresh">Refresh Animation</button>
+  <button id ="logout">Logout</button>
   `;
-  outlet.innerHTML = animationTemplate;
+  outlet.innerHTML = loginTemplate;
+  history.pushState({ page: 1 }, null, "?login");
 
-  let geoKey = "hl5CFvCH0GGgkLuulKNPcQ5q1bPDGvYH";
+  const loginButton = document.getElementById("login");
+  loginButton.addEventListener("click", login);
 
-  navigator.geolocation.getCurrentPosition(function (position) {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    let geoLink = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${geoKey}&location=${lat},${long}&includeRoadMetadata=true&includeNearestIntersection=true`;
+  async function login() {
+    history.pushState({ page: 2 }, null, "?animation");
 
-    fetch(geoLink)
-      .then((r) => r.json())
-      .then((data) => {
-        let res = data.results[0].locations[0];
-        console.log(`${res.adminArea5}, ${res.adminArea3}, ${res.adminArea1} `);
+    const response = await fetch(
+      "https://cs445-project.herokuapp.com/api/login",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: "map",
 
-        let place = document.getElementById("location");
+          password: "123456",
+        }),
+      }
+    );
 
-        place.innerHTML = `${res.adminArea5}, ${res.adminArea3}, ${res.adminArea1} `;
-      });
-  });
+    const result = await response.json();
+    loginToken = result;
+    console.log(loginToken);
 
-  showAnimation();
-}
+    outlet.innerHTML = animationTemplate;
 
-async function showAnimation() {
-  let animation = await fetch(
-    " https://cs445-project.herokuapp.com/api/animation",
-    {
-      method: "GET",
-      headers: { Authorization: `Bearer ${loginToken.token}` },
-    }
-  );
-  let resA = await animation.text();
-  let frame = resA.split("=====\n");
+    let geoKey = "hl5CFvCH0GGgkLuulKNPcQ5q1bPDGvYH";
 
-  let frameCount = 0;
-  window.timerId = setInterval(function () {
-    document.getElementById("animation").innerHTML =
-      frame[frameCount % frame.length];
-    frameCount++;
-  }, 300);
-}
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      let geoLink = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${geoKey}&location=${lat},${long}&includeRoadMetadata=true&includeNearestIntersection=true`;
 
-function refresh() {
-  clearInterval(window.timerId);
-  document.getElementById("animation").innerHTML = "";
-  showAnimation();
-}
+      fetch(geoLink)
+        .then((r) => r.json())
+        .then((data) => {
+          let res = data.results[0].locations[0];
+          console.log(
+            `${res.adminArea5}, ${res.adminArea3}, ${res.adminArea1} `
+          );
 
-function logout() {
-  history.pushState({ page: 2 }, "animation", "?login ");
-  clearInterval(window.timerId);
+          let place = document.getElementById("location");
 
-  outlet.innerHTML = window.loginTemplate;
-}
+          place.innerHTML = `${res.adminArea5}, ${res.adminArea3}, ${res.adminArea1} `;
+        });
+    });
+
+    showAnimation();
+    document.getElementById("refresh").addEventListener("click", refresh);
+    document.getElementById("logout").addEventListener("click", logout);
+  }
+
+  async function showAnimation() {
+    let animation = await fetch(
+      " https://cs445-project.herokuapp.com/api/animation",
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${loginToken.token}` },
+      }
+    );
+    let resA = await animation.text();
+    let frame = resA.split("=====\n");
+
+    let frameCount = 0;
+    window.timerId = setInterval(function () {
+      document.getElementById("animation").innerHTML =
+        frame[frameCount % frame.length];
+      frameCount++;
+    }, 300);
+  }
+
+  function refresh() {
+    clearInterval(window.timerId);
+    document.getElementById("animation").innerHTML = "";
+    showAnimation();
+  }
+
+  function logout() {
+    history.pushState({ page: 2 }, "animation", "?login ");
+    clearInterval(window.timerId);
+
+    outlet.innerHTML = loginTemplate;
+    document.getElementById("login").addEventListener("click", login);
+  }
+};
