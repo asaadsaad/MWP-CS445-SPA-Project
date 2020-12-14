bed1a9e3eb5d211ebea956b56ebf88f677d84318
Bipin2020100 <b.regmi2014@gmail.com>
2020-12-14 07:04:44 -0600

    first day i just did some research and enjoyed my weekend. At this point i am just working on my 2 templates.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// your code here
+const output = document.querySelector("#outlet");
+
+const loginPage =
+`
+<h1>Please login</h1>
+<h3>
+<label for="firstname">First name:</label><br>
+<input type="text" id="firstname" value = "map"/> <br>
+<label for="lastname">Last name:</label><br>
+<input type="text" id="lastname" value = "123456"> <br>
+<button id ="login">Login</button>
+</h3>`
+
+output.innerHTML = loginPage;
+
+const animationPage =
