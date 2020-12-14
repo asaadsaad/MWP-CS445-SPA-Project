6d06a0e10b6e1a531d6b7c1de0506073d8a5e533
efremKidane <69815763+efremKidane@users.noreply.github.com>
2020-12-14 16:51:15 -0600

    set animation in text area and display location

--- script.js
+++ script.js
@@ -3,27 +3,25 @@ const div = document.getElementById("outlet");
 
 window.onload = function login() {
 
-   let template1 = ` <h1>Please Login</h1>
+   let templateLogin = ` <h1>Please Login</h1>
                          Username<input id = "username" /></br>
                          Password<input id = "password"/></br>
                          <button id = "login">Login</button>`;
-   div.innerHTML = template1;
-   
-  
-   document.getElementById("login").onclick = refreshed;
+   div.innerHTML = templateLogin;
 
+
+   document.getElementById("login").onclick = refreshed;
+   
    function refreshed() {
       let userName = document.getElementById("username").value;
       let password = document.getElementById("password").value;
-
+      getToken();
       async function getToken() {
          const state = {
             username: userName, //map
             password: password  //123456
          }
 
-         // console.log(userName);
-         // console.log(password);
          const result = await fetch("https://cs445-project.herokuapp.com/api/login", {
             method: "POST",
             headers: { 'Accept': 'application/json', "content-type": "application/json" },
@@ -32,21 +30,68 @@ window.onload = function login() {
 
          })
          const data = await result.json();
-         let token = data.token;
-         console.log(token);               
+         var token = data.token;
+
          if (data.status) {
             animation();
          }
          async function animation() {
-            let template2 = `<h2> welcome</h2>
-                        <textarea id ="animation" rows = "6" cols = "40"></textarea></br>
+            let templateAnimation = `<h2> welcome to</h2>
+                        <textarea id ="animation" rows = "30" cols = "80"></textarea></br>
                         <button id ="refresh">Refresh </button>
                         <button id ="logout">Logout</button>`;
-            div.innerHTML = template2;
-            document.getElementById("refresh").onclick = getToken;
+            div.innerHTML = templateAnimation;
+
             document.getElementById("logout").onclick = login;
+            document.getElementById("refresh").onclick = function () {
+               clearInterval(intervalID);
+               getToken();
+            }
+            function getLocation() {
+               if (navigator.geolocation) {
+                 navigator.geolocation.getCurrentPosition(showPosition);
+               } 
+             }
+             
+            async function showPosition(position) {
+               var lat = position.coords.latitude;
+               var lng = position.coords.longitude
+               const getGeoLocation = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=vTlxAHpj3BYQzJctWjHLUf8Ht45yyoNT&location=
+               ${lat}, ${lng}&includeRoadMetadata=true&includeNearestIntersection=true`);
+               const location = await getGeoLocation.json();
+               const adress = await location.results[0].locations;
+              document.querySelector("h2").innerHTML += ` ${adress[0].street} ,${adress[0].adminArea5}
+                                                           ,${adress[0].adminArea3} ,${adress[0].postalCode} ,${adress[0].adminArea1}`;
+              console.log(adress);
+             }
+            getLocation();
+             
+            
+            const res = await fetch("https://cs445-project.herokuapp.com/api/animation", {
+               method: "GET",
+               headers: {
+                  'Authorization': `Bearer ${token}`
+               }
+            })
+            console.log(res);
+            const d = await res.text();
+            let x = d.split("=====");
+
+            let curNewsIndex = -1;
+            var intervalID = setInterval(function () {
+               ++curNewsIndex;
+               if (curNewsIndex >= x.length) {
+                  curNewsIndex = 0;
+               }
+               document.getElementById('animation').innerHTML = x[curNewsIndex]
+
+            }, 200);
+
+
          }
+
       }
-      getToken();
+     
    }
+
 }
