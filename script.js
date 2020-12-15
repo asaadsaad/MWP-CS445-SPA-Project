c27a062e76f7cfe6989fa9ab9726c444873f2a1b
Bipin2020100 <b.regmi2014@gmail.com>
2020-12-14 18:09:51 -0600

    worked on login page and location today. I have some work to do on animation. Let's hope i can finish by tomorrow

--- script.js
+++ script.js
@@ -1,16 +1,106 @@
-const output = document.querySelector("#outlet");
+window.onload = function () {
 
-const loginPage =
-`
-<h1>Please login</h1>
+
+  let longitude;
+  let latitude;
+  const output = document.querySelector("#outlet");
+  let loc;
+  let animationPage;
+
+
+  const loginPage =
+    `<h1>Please login</h1>
 <h3>
-<label for="firstname">First name:</label><br>
+<label >First name:</label><br>
 <input type="text" id="firstname" value = "map"/> <br>
-<label for="lastname">Last name:</label><br>
-<input type="text" id="lastname" value = "123456"> <br>
+<label >Password:</label><br>
+<input type="text" id="password" value = "123456"> <br>
 <button id ="login">Login</button>
 </h3>`
+  output.innerHTML = loginPage;
 
-output.innerHTML = loginPage;
 
-const animationPage =
+  const login = document.querySelector("#login");
+
+  login.onclick = async function () { //ask token to Asaad.
+
+    let user = await fetch(`https://cs445-project.herokuapp.com/api/login`, {
+      method: "post",
+      body: JSON.stringify({
+        username: firstname.value,
+        password: password.value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+
+    // console.log(user)
+    const authorize = await user.json();
+    console.log(authorize)
+
+
+    if (authorize.status) {
+      navigator.geolocation.getCurrentPosition(success, fail);
+
+      async function success(position) {
+        longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
+
+        try {
+
+          const geoposition = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=FAL1JLFMUlCJQtNdzgA8WyJtIiayOEDS&location=${latitude},${longitude}`)
+
+          const data = await geoposition.json();
+          console.log(data)
+          // return data;
+
+          document.querySelector("h4").innerHTML = `Welcome all from ${data.results[0].locations[0].adminArea5},${data.results[0].locations[0].adminArea4} ${data.results[0].locations[0].adminArea4Type},${data.results[0].locations[0].adminArea3} ${data.results[0].locations[0].adminArea3Type}, ${data.results[0].locations[0].adminArea1}`
+
+        } catch (error) {
+          
+          console.log(error)
+        }
+
+      }
+
+
+      animationPage =
+        `<h4>Welcome all from ${longitude} and ${latitude}</h4>
+   <textarea id = "animation page" cols = "75" rows = "25"></textarea><br>
+   <button id="refresh">Refresh Animation</button>   <button id ="logout">Logout</button>`
+
+
+      const anime = document.querySelector("#refresh");
+
+      output.innerHTML = animationPage;
+
+      anime.onclick = async function () {
+
+        let animation = await fetch(`https://cs445-project.herokuapp.com/api/animation`, console.log({
+          method: "post",
+          body: JSON.stringify({
+            username: firstname.value,
+            password: password.value
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }));
+
+        const characters = await animation.json();
+        console.log(characters)
+      }
+
+
+      function fail(msg) {
+
+        console.log(msg.code + msg.message); // Log the error
+      }
+    } else {
+      output.innerHTML = "error: Please try again....."
+    }
+
+  }
+}
