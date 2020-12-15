4af5d8ee0d8982f9a7708132f358f153a4d64c3e
Bipin2020100 <b.regmi2014@gmail.com>
2020-12-15 15:57:43 -0600

    updated

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ window.onload = function firstPage() {
 
   let longitude;
   let latitude;
-  let loc;
   let animationPage;
   const output = document.querySelector("#outlet");
 
@@ -36,36 +35,40 @@ window.onload = function firstPage() {
 
     let user = await fetch(`https://cs445-project.herokuapp.com/api/login`, {
       method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
         username: firstname.value,
         password: password.value
-      }), // sending the post request according to the requirement of API
-      headers: {
-        "Content-Type": "application/json"
-      }
+      }) // sending the post request according to the requirement of API
+
     });
 
     // console.log(user) 
     const authorize = await user.json(); //because we are receiving data as json accoriding to the instructions in API
-    console.log(authorize)
+     console.log(authorize)
 
 
     if (authorize.status) {
 
       animationPage =
-        `<h4>Welcome all from ${longitude} and ${latitude}</h4>
-   <textarea id = "animationpage" cols = "75" rows = "25"></textarea><br>
-   <button id="refresh">Refresh Animation</button>   <button id ="logout">Logout</button>` //This is just a layout which can be edited later on as per the requirement to get the details of city and area from the mapQuest website with the help of longitude and latitude we have.
+        `<h4>Welcome to ${longitude}, ${latitude}</h4> 
+        <textarea id = "animationpage" cols = "75" rows = "25"></textarea><br>
+        <button id="refresh">Refresh Animation</button>   <button id ="logout">Logout</button>` //This is just a layout which can be edited later on as per the requirement to get the details of city and area from the mapQuest website with the help of longitude and latitude we have.
+
+        //In this beginning I wrote longitude and latitude in the innerHTML textboxjust to see if that is printed in innerHTML. Later on I am going to change this tag fetching the city name and full address with the help of latitude and longitude
 
       output.innerHTML = animationPage;
 
       const anime = document.querySelector("#refresh");
       const stopanime = document.querySelector("#logout");
+      let loop;
 
       /**
        * This function is to get the request for string that is needed to workout on the animation.
        */
-      let loop;
+      
       anime.onclick = async function star() {
 
         let animation = await fetch(`https://cs445-project.herokuapp.com/api/animation`, {
@@ -74,7 +77,8 @@ window.onload = function firstPage() {
             "Authorization": "Bearer " + authorize.token
           }
         });
-
+        console.log(authorize.token)
+        // console.log(animation)
         const characters = await animation.text();
         // console.log(characters)
 
@@ -84,11 +88,11 @@ window.onload = function firstPage() {
         let currentPos = 0;
         animationpage.innerHTML = frames[0];
 
-       loop = setInterval(() => {
+        loop = setInterval(() => {
           // currentPos;
           animationpage.innerHTML = frames[currentPos++];
-          if (currentPos === x ) {
-            currentPos = 0
+          if (currentPos === x) {
+            currentPos = 0;
           }
         }, 200);
 
@@ -99,7 +103,7 @@ window.onload = function firstPage() {
           star();
         }
 
-        stopanime.onclick = function(){
+        stopanime.onclick = function () {
           clearInterval(loop);
           firstPage();
         }
@@ -120,11 +124,19 @@ window.onload = function firstPage() {
 
           const geoposition = await fetch(`http://www.mapquestapi.com/geocoding/v1/reverse?key=FAL1JLFMUlCJQtNdzgA8WyJtIiayOEDS&location=${latitude},${longitude}`)
 
+
+          // console.log(geoposition);
           const data = await geoposition.json();
-          // console.log(data)
+          // console.log(data);
           // return data;
 
-          document.querySelector("h4").innerHTML = `Welcome all from ${data.results[0].locations[0].adminArea5},${data.results[0].locations[0].adminArea4} ${data.results[0].locations[0].adminArea4Type},${data.results[0].locations[0].adminArea3} ${data.results[0].locations[0].adminArea3Type}, ${data.results[0].locations[0].adminArea1}`
+          const city = data.results[0].locations[0].adminArea5 ;
+          const county = data.results[0].locations[0].adminArea4;
+          // const countyType = data.results[0].locations[0].adminArea4Type;
+          const state = data.results[0].locations[0].adminArea3;
+          const country = data.results[0].locations[0].adminArea1;
+
+          document.querySelector("h4").innerHTML = `Welcome all from ${city}, ${county} county, ${state} state, ${country}`
 
         } catch (error) { //if the location data cannot be fatched
 
@@ -132,7 +144,6 @@ window.onload = function firstPage() {
         }
 
       }
-
 
       function fail(msg) { // this is the fail function for navigator.geolocation.getCurrentPosition method
 
