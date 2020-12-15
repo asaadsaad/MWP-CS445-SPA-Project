65abb8c1d8123959fde0094720bec31efdf7c2a1
jchronister <69609933+jchronister@users.noreply.github.com>
2020-12-14 19:53:03 -0600

    Added Location/History

--- script.js
+++ script.js
@@ -11,13 +11,22 @@
                 case "class":
                     el.classList.add(add);
                     break;
-                // case "text":
-                //   el.innerText = add;
-                //   break;
-                case "source":
-                    el.src = add;
-                    break;
                 default:
+                    //****************** typing error 
+                    // interface I1 {
+                    //   readonly n: number
+                    //   s: string
+                    // }
+                    // type IfEquals<X, Y, A=X, B=never> =
+                    // (<T>() => T extends X ? 1 : 2) extends
+                    // (<T>() => T extends Y ? 1 : 2) ? A : B;
+                    // type WritableKeys<T> = {
+                    //   [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
+                    // }[keyof T];
+                    // type I2 = Pick<I1, WritableKeys<I1>>; 
+                    // equivalent to { s: string; }
+                    // el[type as I2] = add;
+                    // el[type as keyof WritableKeys<HTMLElement>] = add;
                     el[type] = add;
             }
         });
@@ -33,12 +42,12 @@
         // Username
         let subEl = createElmt("div", el, [["class", "margin"]]);
         createElmt("label", subEl, [["innerText", "Username:"]]);
-        let user = createElmt("input", subEl, [["type", "text"]]);
+        let user = createElmt("input", subEl, [["type", "text"], ["value", "map"]]);
         // Password
         subEl = createElmt("div", el, [["class", "margin"]]);
         createElmt("label", subEl, [["innerText", "Password:"]]);
         //******* CHANGE TO PASSWORD ["type","password"]
-        let pw = createElmt("input", subEl, [["type", "text"]]);
+        let pw = createElmt("input", subEl, [["type", "text"], ["value", "123456"]]);
         // Login Button
         subEl = createElmt("div", el, [["class", "center"], ["class", "margin"]]);
         createElmt("button", subEl, [["innerText", "Login"]]).onclick = function () {
@@ -66,60 +75,98 @@
         };
         return frag;
     };
-    let createAnimationPage = function () {
+    let createAnimationPage = function (updtFromHistory) {
         let frag = document.createDocumentFragment();
         // Header
         let el = createElmt("div", frag, [["class", "centered"]]);
-        createElmt("h2", el, [["class", "center"], ["innerText", "Welcome"]]);
+        let welcome = createElmt("h2", el, [["class", "center"], ["innerText", "Welcome"]]);
+        // Get Location
+        if (updtFromHistory) {
+            welcome.innerText = displayAnimation.location;
+        }
+        else {
+            navigator.geolocation.getCurrentPosition(function ({ coords: { latitude: lat, longitude: long } }) {
+                //**Key Goes Here
+                HTTPRequest(`http://www.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${lat},${long}`)
+                    .then(n => n.json())
+                    .then((n) => {
+                    let { results } = n;
+                    let { locations } = results[0];
+                    let { adminArea1: country, adminArea3: state, adminArea5: city } = locations[0];
+                    displayAnimation.location = `Welcome: from ${city}, ${state}, ${country} Thank You for Visiting`;
+                    welcome.innerText = displayAnimation.location;
+                });
+            }, 
+            // Get Location Error
+            () => { welcome.innerText = `Welcome: From ? Location Unavailable`; });
+        }
         // Text Area
         let subEl = createElmt("div", el, [["class", "margin"]]);
-        let txtArea = createElmt("textarea", subEl, []);
-        // Refresh Button
+        let txtArea = createElmt("textarea", subEl, [["class", "animation"]]);
+        // Load Animation Button
         subEl = createElmt("div", el, [["class", "center"], ["class", "margin"]]);
-        let refreshBtn = createElmt("button", subEl, [["innerText", "Refresh"]]);
-        refreshBtn.onclick = function () {
+        let refreshBtn = createElmt("button", subEl, [["innerText", "Load Animation"], ["class", "margin"]]);
+        let refreshAnimation = function () {
             let animationReq = {
                 method: "GET",
                 headers: { "Authorization": "Bearer " + userToken }
             };
-            // console.log("https://cs445-project.herokuapp.com/api/animation?Bearer=" + userToken)
-            HTTPRequest("https://cs445-project.herokuapp.com/api/animation", animationReq)
-                .then(n => n.text())
-                .then(n => {
-                displayAnimation.setimage(n);
-                displayAnimation.animate(txtArea);
-            });
+            let tries = 0;
+            let request = function () {
+                HTTPRequest("https://cs445-project.herokuapp.com/api/animation", animationReq)
+                    .then(n => n.text())
+                    .then(n => {
+                    if (n === displayAnimation.imageStr && tries++ < 5) {
+                        // Duplicate - Request Again
+                        request();
+                    }
+                    else {
+                        // Display Animation
+                        tries = 0;
+                        displayAnimation.setimage(n);
+                        displayAnimation.animate(txtArea);
+                    }
+                });
+            };
+            request();
         };
+        refreshBtn.onclick = refreshAnimation;
+        // Refresh for History Changes
+        if (updtFromHistory)
+            displayAnimation.animate(txtArea);
         // Logout Button
-        createElmt("button", subEl, [["innerText", "Logout"]]).onclick = function () {
+        createElmt("button", subEl, [["innerText", "Logout"], ["class", "margin"]]).onclick = function () {
             loadLoginPage();
             userToken = "";
+            displayAnimation.clearAnimation();
             this.onclick = null;
             refreshBtn.onclick = null;
         };
         return frag;
     };
-    let loadLoginPage = function () {
+    let loadLoginPage = function (fromHistory = false) {
         clearPage();
-        addHistory("login");
+        if (!fromHistory)
+            addToHistory("login");
         div.appendChild(createLoginPage());
     };
-    let loadAnimationPage = function () {
+    let loadAnimationPage = function (fromHistory = false) {
         clearPage();
-        addHistory("animation");
-        div.appendChild(createAnimationPage());
+        div.appendChild(createAnimationPage(fromHistory));
+        if (!fromHistory)
+            addToHistory("animation");
     };
     let clearPage = function () {
         div.firstChild.remove();
     };
-    let addHistory = function (urlAdd) {
-        history.pushState({}, "", "/" + urlAdd);
+    let addToHistory = function (urlAdd) {
+        history.pushState({ display: urlAdd }, "", "/" + urlAdd);
     };
-    // Object to Handle Animation
     let displayAnimation = {
         imageStr: "",
         imageAry: [""],
-        timerId: null,
+        timerId: -1,
+        location: "",
         setimage(img) {
             this.imageStr = img;
             this.imageAry = img.split("=====\n");
@@ -130,30 +177,25 @@
             let length = this.imageAry.length;
             if (length === 0)
                 return;
-            this.timerId = setInterval(() => {
+            this.timerId = window.setInterval(() => {
                 elmt.value = this.imageAry[cnt++];
                 cnt = cnt % length;
             }, 200);
         },
         clearAnimation() {
-            if (this.timerId !== null) {
+            if (this.timerId >= 0) {
                 clearInterval(this.timerId);
-                this.timerId = null;
+                this.timerId = -1;
             }
             ;
         }
     };
     //************* responsetype optional
     // Fetch Requests
-    let HTTPRequest = async function (url, data, responseType) {
+    let HTTPRequest = async function (url, data) {
         try {
             const response = await fetch(url, data);
             return response;
-            // if (responseType === "json") {
-            //   // return response.json();
-            // } else {
-            //   return response.text();
-            // }
         }
         catch (err) {
             alert("An Error has Occurred. Please See Console for Details\n" + err);
@@ -165,4 +207,35 @@
         div = document.getElementById("outlet");
         loadLoginPage();
     };
+    // History
+    window.addEventListener("popstate", function ({ state }) {
+        if (state && state.display === "animation") {
+            if (userToken) {
+                loadAnimationPage(true); // Currently Logged In
+                return;
+            }
+            else {
+                window.alert("Already Logged Out. Please Login Again!");
+            }
+        }
+        // Load Login, Push /login if Needed
+        loadLoginPage(state !== null);
+    });
+    // CSS Styles
+    let styles = [".margin {margin: 10px;}",
+        ".center {text-align: center;}",
+        `.centered {
+                    position: fixed;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                }`,
+        `.animation {
+                      width: 450px;
+                      height: 350px;
+                 }`,
+    ];
+    // Create/ Append Stylesheet and Insert Rules
+    let style = createElmt("style", document.head, [["type", "text/css"]]).sheet;
+    styles.forEach(n => style.insertRule(n, 0));
 }());
--- script.ts
+++ script.ts
@@ -11,7 +11,7 @@
 
   // Create New Element and Add Properties. Append to Element
   let createElmt = function (elmtType: string, elmt: HTMLElement|DocumentFragment|null, InfoAdd: string[][]): HTMLElement {
-    
+ 
     let el = document.createElement(elmtType)
 
     // Add Info to Created Element
@@ -23,13 +23,25 @@
           el.classList.add(add);
           break;
 
-        // case "text":
-        //   el.innerText = add;
-        //   break;
-        case "source":
-          (<HTMLImageElement>el).src = add;
-          break;
         default:
+        //****************** typing error 
+          // interface I1 {
+          //   readonly n: number
+          //   s: string
+          // }
+
+          // type IfEquals<X, Y, A=X, B=never> =
+          // (<T>() => T extends X ? 1 : 2) extends
+          // (<T>() => T extends Y ? 1 : 2) ? A : B;
+
+          // type WritableKeys<T> = {
+          //   [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
+          // }[keyof T];
+
+          // type I2 = Pick<I1, WritableKeys<I1>>; 
+          // equivalent to { s: string; }
+          // el[type as I2] = add;
+          // el[type as keyof WritableKeys<HTMLElement>] = add;
           el[type] = add;
       }
       
@@ -52,14 +64,14 @@
     // Username
     let subEl = createElmt("div", el, [["class","margin"]])
     createElmt("label", subEl, [["innerText","Username:"]])
-    let user = <HTMLInputElement>createElmt("input", subEl, [["type","text"]])
+    let user = <HTMLInputElement>createElmt("input", subEl, [["type","text"], ["value","map"]])
 
     // Password
     subEl = createElmt("div", el, [["class","margin"]])
     createElmt("label", subEl, [["innerText","Password:"]])
 
     //******* CHANGE TO PASSWORD ["type","password"]
-    let pw = <HTMLInputElement>createElmt("input", subEl, [["type","text"]])
+    let pw = <HTMLInputElement>createElmt("input", subEl, [["type","text"], ["value","123456"]])
 
     // Login Button
     subEl = createElmt("div", el, [["class","center"], ["class","margin"]])
@@ -71,7 +83,7 @@
         body: JSON.stringify({username: user.value, password: pw.value}),
         headers: {"Content-Type": "application/json"}
       }
-      
+
       HTTPRequest("https://cs445-project.herokuapp.com/api/login", loginReq)
         .then((n)=>n.json())  
         .then(({status, token}) => {
@@ -99,42 +111,84 @@
   }
 
 
-  let createAnimationPage = function (): DocumentFragment {
+  let createAnimationPage = function (updtFromHistory: boolean): DocumentFragment {
 
     let frag = document.createDocumentFragment();
 
     // Header
     let el = createElmt("div", frag, [["class","centered"]])
-    createElmt("h2", el, [["class","center"], ["innerText","Welcome"]])
+    let welcome = createElmt("h2", el, [["class","center"], ["innerText","Welcome"]])
 
+    // Get Location
+    if (updtFromHistory) {
+      welcome.innerText = displayAnimation.location;
+    } else {
+
+        navigator.geolocation.getCurrentPosition(function({coords: {latitude: lat, longitude: long}}) {
+
+          //**Key Goes Here
+          
+          HTTPRequest(`http://www.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${lat},${long}`)
+          .then(n=>n.json())
+          .then((n)=>{
+            
+            let {results} = n;
+            let {locations} = results[0];
+            let {adminArea1: country, adminArea3: state, adminArea5: city} = locations[0];
+            displayAnimation.location = `Welcome: from ${city}, ${state}, ${country} Thank You for Visiting`;
+            welcome.innerText = displayAnimation.location;
+            
+          })
+
+        },
+          // Get Location Error
+          ()=>{welcome.innerText = `Welcome: From ? Location Unavailable`;}
+        );
+  }
+  
     // Text Area
     let subEl = createElmt("div", el, [["class","margin"]])
-    let txtArea = createElmt("textarea", subEl, [])
+    let txtArea = <HTMLTextAreaElement>createElmt("textarea", subEl, [["class","animation"]])
 
-    // Refresh Button
+    // Load Animation Button
     subEl = createElmt("div", el, [["class","center"], ["class","margin"]])
-    let refreshBtn = createElmt("button", subEl, [["innerText","Refresh"]])
+    let refreshBtn = createElmt("button", subEl, [["innerText","Load Animation"], ["class","margin"]])
     
-    refreshBtn.onclick = function() {
+    let refreshAnimation = function () {
 
       let animationReq = {
         method: "GET",
         headers: {"Authorization": "Bearer " + userToken}
       }
-      // console.log("https://cs445-project.herokuapp.com/api/animation?Bearer=" + userToken)
-      HTTPRequest("https://cs445-project.herokuapp.com/api/animation", animationReq)
-      .then(n=>n.text())
-      .then(n=>{
-        displayAnimation.setimage(n);
-        displayAnimation.animate(txtArea);
-      })
-      
-      
-      
+
+      let tries = 0;
+
+      let request = function () {
+
+        HTTPRequest("https://cs445-project.herokuapp.com/api/animation", animationReq)
+        .then(n=>n.text())      
+        .then(n=>{ 
+          if(n === displayAnimation.imageStr && tries++ < 5) {
+            // Duplicate - Request Again
+            request();
+          } else {
+            // Display Animation
+            tries = 0;
+            displayAnimation.setimage(n);
+            displayAnimation.animate(txtArea);
+          }
+        })
+      }
+      request();
     }
 
+    refreshBtn.onclick = refreshAnimation;
+
+    // Refresh for History Changes
+    if (updtFromHistory) displayAnimation.animate(txtArea);
+
     // Logout Button
-    createElmt("button", subEl, [["innerText","Logout"]]).onclick = function() {
+    createElmt("button", subEl, [["innerText","Logout"], ["class","margin"]]).onclick = function() {
       loadLoginPage();
       userToken = "";
       displayAnimation.clearAnimation();
@@ -146,20 +200,19 @@
 
   }
 
-
-  let loadLoginPage = function(): void {
+  let loadLoginPage = function(fromHistory: boolean = false): void {
 
     clearPage();
-    addHistory("login")
+    if (!fromHistory) addToHistory("login");
     div.appendChild(createLoginPage());
 
   }
 
-  let loadAnimationPage = function(): void {
+  let loadAnimationPage = function(fromHistory: boolean = false): void {
 
-    clearPage();   
-    addHistory("animation")
-    div.appendChild(createAnimationPage());
+    clearPage();
+    div.appendChild(createAnimationPage(fromHistory));
+    if (!fromHistory) addToHistory("animation");
 
   }
 
@@ -167,25 +220,17 @@
     (<HTMLElement>div.firstChild).remove();
   }
 
-  let addHistory = function (urlAdd: string): void {
-    history.pushState({},"","/" + urlAdd);
+  let addToHistory = function (urlAdd: string): void {
+    history.pushState({display: urlAdd}, "", "/" + urlAdd);
   }
 
-  // Object to Handle Animation
-  let displayAnimation : {
-    
-    imageStr: string,
-    imageAry: string[],
-    timerId: null | number,
-    animate(elmt: HTMLTextAreaElement): void,
-    clearAnimation(): void,
-    setimage(img: string): void
-
-  } = {
+  
+  let displayAnimation = {
 
     imageStr: "",
     imageAry: [""],
-    timerId: null,
+    timerId: -1,
+    location: "",
 
     setimage (img: string): void {
       this.imageStr = img;
@@ -201,7 +246,7 @@
 
       if (length === 0) return;
 
-      this.timerId = setInterval(()=>{
+      this.timerId = window.setInterval(()=>{
 
         elmt.value = this.imageAry[cnt++];
         cnt = cnt % length;
@@ -211,43 +256,76 @@
     },
 
     clearAnimation(): void {
-      if(this.timerId !== null) {
-       clearInterval(this.timerId); 
-       this.timerId = null
-      };
+        if(this.timerId >= 0) {
+          clearInterval(this.timerId); 
+          this.timerId = -1;
+        };
     }
 
   }
 
 //************* responsetype optional
   // Fetch Requests
-  let HTTPRequest = async function(url: string, data?: object, responseType?: string): Promise<loginResponse | string> {
+  let HTTPRequest = async function(url: string, data?: object): Promise<any> {
     
     try {
-
-      const response = await fetch(url,data);
-      return response;
-      // if (responseType === "json") {
-      //   // return response.json();
-      // } else {
-      //   return response.text();
-      // }
-
+        const response = await fetch(url,data);
+        return response;
     } catch (err) {
-      alert("An Error has Occurred. Please See Console for Details\n" + err);
-      return err;
+        alert("An Error has Occurred. Please See Console for Details\n" + err);
+        return err;
     }
     
   }
 
 
-
   // Initialize
   window.onload = ()=>{
-    div = <HTMLElement>document.getElementById("outlet");
-   
-    loadLoginPage();
-
+      div = <HTMLElement>document.getElementById("outlet");
+      loadLoginPage();
   }
+
+  // History
+  window.addEventListener("popstate", function({state}:PopStateEvent) {
+
+      if (state && state.display === "animation") {
+
+          if (userToken) {
+              loadAnimationPage(true); // Currently Logged In
+              return;
+          } else {
+              window.alert("Already Logged Out. Please Login Again!");
+          }
+        
+      } 
+      
+      // Load Login, Push /login if Needed
+      loadLoginPage(state !== null)
+    
+  });
+
+
+  // CSS Styles
+  let styles = [ ".margin {margin: 10px;}",
+                 ".center {text-align: center;}",
+                 `.centered {
+                    position: fixed;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                }`,
+                `.animation {
+                      width: 450px;
+                      height: 350px;
+                 }`,
+               ]
+
+  // Create/ Append Stylesheet and Insert Rules
+  let style = createElmt("style",document.head, [["type", "text/css"]]).sheet;
+  styles.forEach(n=>style.insertRule(n, 0));
+   
+
+
+
 
 }())
