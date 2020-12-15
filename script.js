eba980cc096fadd449fc5c0056e25138d6209687
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2020-12-15 00:04:05 -0600

    authentication user token feature added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 "use strict";
 /* eslint-disable */
 
+window.onload = application;
 
-window.onload = application
 function application() {
+    let token;
+
     let loginPage = ` 
     <h1> Please login </h1>
     UserName: <input id="UserName" type="text" value= "mwp"></br>
@@ -16,23 +18,28 @@ function application() {
     <button type="button" id="refresh">Refresh Animation</button>
     <button type="button" id="logout">Logout</button>`;
 
+
+
     let div = document.querySelector("#outlet");
     div.innerHTML = loginPage;
+    history.pushState({ page: 1 }, "login", "?/login ");
 
     let login = document.querySelector("#login")
     login.addEventListener("click", loginFunc);
+    login.addEventListener("click", () => history.pushState({ page: 2 }, "animation", "?/animation "));
 
 
 
     function loginFunc() {
         div.innerHTML = animationPage;
         login = true;
-        addEvent()
+        addEvent();
+        authenticateUserToken();
     };
 
     function addEvent() {
         if (login) {
-            let header=document.querySelector("#locationstatus")
+            let header = document.querySelector("#locationstatus")
             let text = document.querySelector("#textarea");
             let Refresh_Animation = document.querySelector("#refresh");
             let logout = document.querySelector("#logout");
@@ -44,9 +51,35 @@ function application() {
     function logoutFunc() {
         // console.log("hamid")
         login = false;
-        addEvent();
+        // addEvent();
         div.innerHTML = loginPage;
         window.location.reload();
     };
+
+
+
+    async function authenticateUserToken() {
+
+        const result = await fetch('https://cs445-project.herokuapp.com/api/login',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: "mwp",password:"123"})
+            });
+
+        const myJson = await result.json()
+        console.log(myJson);
+        token = myJson.token;
+        console.log(token);
+        const status = myJson.status;
+        if (status === true) {
+            mytest();
+        }
+    }
+
+    function mytest() {
+        console.log("perfect");
+    }
+
 
 }
