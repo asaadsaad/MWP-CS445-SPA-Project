7a28fab011e21093bbb3c2ef68237c7db3af6605
luwites <luwamtes111@gmail.com>
2020-12-15 19:05:55 -0600

    fourth changes made

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 let tok = {
     token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZWNyZXQiOiJNb2Rlcm4gQXN5bmNocm9ub3VzIFByb2dyYW1taW5nIiwiaWF0IjoxNjA3OTg0MzEzfQ.zdugpxz3FKBdEXbf0nuXeDhI1zVzqrXYDv_OmYkzCbA",
     status: true
-  }
-  
+}
+
 window.onload = function login() {
     let loginTemplate = `<h1>Login Please</h1>
     <input type="text" id="username" placeholder="User name" required /><br><br>
     <input type="text" id="password" placeholder="Passwod" required /><br><br>
     <button type="button" id="loginButton" class="btn btn-primary btn-lg">Login</button>`;
     document.querySelector('div').innerHTML = loginTemplate;
+    
 
     document.getElementById('loginButton').onclick = async function log() {
         let inputsObj = {}
@@ -27,40 +28,37 @@ window.onload = function login() {
             body: JSON.stringify(inputsObj)
 
         })
-         
+
             .then(response => response.json())
-        
+
             .then((response) => { response.status === true ? animationPage() : login() })
- 
+
         function animationPage() {
             let animationTemplate = `<h1>Welcome</h1>
             <textarea id="animation" name="anim" rows="20" cols="50"></textarea><br><br>
-            <button type="button" id="resfreshAnimation" class="btn btn-primary btn-lg">Refresh Animation</button>
+            <button type="button" id="refreshAnimation" class="btn btn-primary btn-lg">Refresh Animation</button>
             <button type="button" id="logout" class="btn btn-primary btn-lg">Logout</button>`;
-            document.querySelector('div').innerHTML = animationTemplate
-           
+            document.querySelector('div').innerHTML = animationTemplate;
+            document.getElementById('refreshAnimation').onclick = refreshing;
+            document.getElementById('logout').onclick = loggingOut;
         }
         getLocation()
+        
     }
 
     function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            console.log("sorry...");
-        }
+        navigator.geolocation.getCurrentPosition(showPosition);
     }
-    function showPosition(position) {
 
+    function showPosition(position) {
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         // console.log(latitude)
         // console.log(longitude)
-         greeterWithLocation()
-         animFetch()
-    
+        greeterWithLocation()
+        animFetch()
     }
-    
+
     async function greeterWithLocation() {
         let loc = await fetch('http://open.mapquestapi.com/geocoding/v1/reverse?key=soAPyBnsVHIA5KhbILyUTH4vDfYI1Tno&location=41.013567099999996,-91.95915409999999&includeRoadMetadata=true&includeNearestIntersection=true')
         // console.log(loc)
@@ -69,26 +67,48 @@ window.onload = function login() {
         let cityStateCountry = parsedloc.results[0].locations[0].adminArea5 + ", " + parsedloc.results[0].locations[0].adminArea3 + ", " + parsedloc.results[0].locations[0].adminArea1 + "!";
         document.querySelector('h1').innerHTML = `<h4>Welcome all from ${cityStateCountry}</h4>`
     }
-    
-    let animFetch= async function() {
-
-        let feData = await fetch('https://cs445-project.herokuapp.com/api/animation',{
+    let timerid;
+    let animArray;
+    let animFetch = async function () {
+        let fetchedFrames = await fetch('https://cs445-project.herokuapp.com/api/animation', {
             method: 'GET',
-
             headers: {
                 'Authorization': 'Bearer ' + tok.token,
-              
-                
             },
         })
-        let recievedAnim = await feData.text();
-        let animArray = recievedAnim.split("=====");
-        //console.log(animArray[1])
-        document.querySelector('textarea').innerHTML = animArray;
-        document.getElementById('logout').onclick = function() {
-        document.querySelector('div').innerHTML = loginTemplate;
-        
-        }
+        let recievedAnimation = await fetchedFrames.text();
+        animArray = recievedAnimation.split("=====\n");
+        let tempIndex = 0
+        timerid = setInterval(function () {
+            document.getElementById("animation").innerHTML = animArray[tempIndex]
+            tempIndex++;
+            if (tempIndex === animArray.length) {
+                tempIndex = 0
+            }
+
+        }, 200)
+
     }
     
+      function refreshing() {
+          clearInterval(timerid);
+          document.querySelector('textarea').innerHTML = animFetch();
+      }
+    
+     function loggingOut() {
+         clearInterval(timerid)
+         document.querySelector('div').innerHTML = loginTemplate;
+     }
+
 }
+
+// document.getElementById('logout').onclick = function() {
+//     document.querySelector('div').innerHTML = loginTemplate;
+
+// } 
+
+// function s(params) {
+//     document.getElementById('refreshAnimation').onclick = function nam() {
+//         document.querySelector('textarea').innerHTML = animFetch; 
+//     }
+// }
