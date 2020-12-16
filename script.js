7d260952dc302e8b644f00a98f97cbb3ccdd7714
tahirmama351 <tahirmama@gmail.com>
2020-12-15 19:08:55 -0600

    I am working on hitroy API

--- script.js
+++ script.js
@@ -2,44 +2,44 @@
 window.onload = function () {
 
      let loginTemplate = `
-     <div >
+     <div style="margin: 50px; height: 220px; width:350px; background-color: MediumSeaGreen;">
      <p style="font-size: 30px"> <b> Login Here </p>
      
-     User Name: <input  id="username" type="text"  placeholder="tahir" value="tahir"> <br>
-     Password: <input id="password" type="password"  placeholder="123456" value="123456"> <br><br>
+     User Name: <input  id="username" type="text"  placeholder="tahir" value="mwp"> <br>
+     Password: <input id="password" type="password"  placeholder="123456" value="123"> <br><br>
      <input  id="login" type="button" value="Login"> 
      </div>`
 
      document.getElementById("outlet").innerHTML = loginTemplate;
 
      let animationTemplate = `
-      <div id="animationPage">
+      <div id="animationPage" style="height: 420px; width:450px; background-color: MediumSeaGreen;  margin: 50px">
      
           <p id="welcomming" style="font-size: 20px"> </p>
 
-          <div id="animationArea">
-          <textarea disabled id="animationTextArea" style="height: 290px; width:300px"></textarea> <br>
+          <div id="animationArea" style="padding-right: 20px; margin: 20px">
+          <textarea disabled id="animationTextArea" style="height: 320px; width:390px"></textarea> <br>
           <input type="button"  id="refreshAnimation" value="Refresh Animation">
           <input type="button" id="logoutBotton" value="Logout"  >
           </div>
      </div>`
 
      document.getElementById("login").addEventListener("click", displayAnimaTemplate);
+   
 
+     function displayAnimaTemplate() {         
 
-     function displayAnimaTemplate() {
+          document.getElementById("outlet").innerHTML = animationTemplate;          
 
-          document.getElementById("outlet").innerHTML = animationTemplate;
+          fetchData()         
 
-          fetchData()
      }
 
 
-
-
-     let token;
-     async function fetchData() {
-          const response = await fetch("https://cs445-project.herokuapp.com/api/login", {
+     let animationId;
+     async function fetchData() {   //feching token and animation
+          // fetch takent state
+          const fetchToken = await fetch("https://cs445-project.herokuapp.com/api/login", {
                method: 'POST',
                headers: { "Content-Type": "application/json" },
                body: JSON.stringify({
@@ -47,14 +47,37 @@ window.onload = function () {
                     password: '123456'
                })
           })
-          const respondBody = await response.json();
-          token = respondBody.token;
+          const respondToken = await fetchToken.json();
+          const token = respondToken.token;
+          // console.log(token)
+
+
+          // fetch animation
+          const fetchAnimation = await fetch("https://cs445-project.herokuapp.com/api/animation",
+               {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${token}` }
+               })
+          let aminationRespond = await fetchAnimation.text();
+          aminationRespond = aminationRespond.split("=====\n");
          
+          //console.log(aminationRespond)
+
+
+          // display anination 
+
+          let frameLength = 0;
+          animationId = setInterval(function () {
+               document.getElementById("animationTextArea").innerHTML = aminationRespond[frameLength];
+               frameLength++;
+               if (frameLength == aminationRespond.length) { frameLength = 0 };
+
+          }, 200);
+
           getGeoLocation()
-        
+
      }
 
-     let currentLocation;
 
      function getGeoLocation() {
 
@@ -70,13 +93,25 @@ window.onload = function () {
                let city = locationInfo.results[0].locations[0].adminArea5;
                let state = locationInfo.results[0].locations[0].adminArea3;
                let country = locationInfo.results[0].locations[0].adminArea1;
-               currentLocation = `You are in ${city},  ${state}, ${country}`;
+               let currentLocation = `You are in ${city},  ${state}, ${country}`;
 
                document.getElementById("welcomming").innerHTML = currentLocation;
           }
           function fail(details) {
                console.log(details.message);
           }
+
+          // refresh animation
+          document.getElementById("refreshAnimation").addEventListener("click", function () { clearInterval(animationId); displayAnimaTemplate() });
+
+          // logout
+          document.getElementById("logoutBotton").addEventListener("click", function () {
+
+               clearInterval(animationId);
+               document.getElementById("outlet").innerHTML = loginTemplate;
+
+               // login again befor refreshing the window             
+               document.getElementById("login").addEventListener("click", displayAnimaTemplate);
+          });
      }
-     
 }
