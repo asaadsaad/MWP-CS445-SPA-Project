03f4439754d39a975a624b14ab43f5dbfbb117a4
mbelay7835 <69566836+mbelay7835@users.noreply.github.com>
2020-12-17 15:29:35 -0600

    I have completed SPA project

--- script.js
+++ script.js
@@ -40,22 +40,27 @@ function spa() {
     const geoKey = "opPTwim7J6IURz5wAUrMZ9cmrvn8HXxq";
     const outlet = document.getElementById("outlet");
 
+    history.pushState({ page: "login" }, "login", "?login");// adding login page to the browser's session history stack
     outlet.innerHTML = loginTemplate;
 
     ////////////////////////////////////////////////////////////////////Geolocation fetch
-    async function location() {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            latitud = position.coords.latitude;
+
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(success)
+        async function success(position) {
             longitud = position.coords.longitude;
-        })
-
-        let fetchJson = await fetch(`https://www.mapquestapi.com/geocoding/v1/reverse?key=${geoKey}&location=41.00704387603765,-91.96811967883806&includeRoadMetadata=true&includeNearestIntersection=true`);
-        let fetchObj = await fetchJson.json();
-        let adress = fetchObj.results[0].locations[0]
-        fullAdress = `Welcome all from ${adress.adminArea5}, ${adress.adminArea3}, ${adress.adminArea1}! `
-        document.getElementById('location').innerHTML = fullAdress
+            latitud = position.coords.latitude;
+            try {
+                let fetchJson = await fetch(`https://www.mapquestapi.com/geocoding/v1/reverse?key=${geoKey}&location=${latitud},${longitud}&includeRoadMetadata=true&includeNearestIntersection=true`)
+                let fetchObj = await fetchJson.json();
+                let adress = fetchObj.results[0].locations[0]
+                fullAdress = `Welcome all from ${adress.adminArea5}, ${adress.adminArea3}, ${adress.adminArea1}! `
+                document.getElementById('location').innerHTML = fullAdress
+            } catch (err) {
+                outlet.innerHTML = err
+            }
+        }
     }
-
 
 
     const logInButton = document.getElementById("login");
@@ -66,40 +71,50 @@ function spa() {
 
 
     async function activitiesAfterLogin() {
+        history.pushState({ page: "animation" }, "animation", "?animation")//adding animation page to the browser's session history stack
         outlet.innerHTML = animationTemplate;
-        location()
-
-        const response = await fetch("https://cs445-project.herokuapp.com/api/login", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify({
-                "username": "map",
-                "password": "123456"
+        getLocation();
+        try {
+            const response = await fetch("https://cs445-project.herokuapp.com/api/login", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify({
+                    "username": "map",
+                    "password": "123456"
+                })
             })
-        })
+            const jsonObj = await response.json()
+            token = jsonObj.token;
+            const status = jsonObj.status;
 
-        const jsonObj = await response.json()
-        token = jsonObj.token;
-        const status = jsonObj.status;
-
-        if (status === true) {
-            getAnimation();
+            if (status === true) {
+                getAnimation();
+            }
+        } catch (err) {
+            outlet.innerHTML = err
         }
+
 
         const animation = document.getElementById("showAnimation");
         async function getAnimation() {
-            const response = await fetch("https://cs445-project.herokuapp.com/api/animation", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/text",
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            const allAnimation = await response.text();
-            const animArray = allAnimation.split("=====\n");
-            // animation.innerHTML = animArray[0];
+            let response;
+            let animArray;
+
+            try {
+                response = await fetch("https://cs445-project.herokuapp.com/api/animation", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/text",
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                const allAnimation = await response.text();
+                animArray = allAnimation.split("=====\n");
+            } catch (err) {
+
+            }
 
             let currentAnim = 0;
             let maxlength = animArray.length;
@@ -111,6 +126,7 @@ function spa() {
                 }
             }, 200);
         }
+
         ////////////////////Refresh the page
         const refresh = document.getElementById("refAnimationBTN");
         refresh.addEventListener("click", clearLastAnimation)
@@ -118,7 +134,8 @@ function spa() {
             clearInterval(moveAnimation)
             getAnimation(); //Function that fetches the animation from the API
         }
-        ///////////////////Log outzthe page
+
+        ///////////////////Log out the page
         const logOut = document.getElementById("logout")
         logOut.addEventListener("click", logoutAnimPage);
         function logoutAnimPage() { //Function that return the login page 
@@ -127,4 +144,17 @@ function spa() {
             logInButton.addEventListener("click", logInFunction);
         }
     }
+
+
+    window.addEventListener("popstate", (event) => {
+        if (event.state.page === "login") {
+            clearInterval(moveAnimation);
+            spa()
+        } else {
+            clearInterval(moveAnimation);
+            activitiesAfterLogin();
+        }
+
+    })
+
 }
